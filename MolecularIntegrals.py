d8aa4148b48773bc69c33139fe967541099dad80
Melisius <erikkjellgren@live.dk>
2017-03-24 02:14:18 +0100

    Add files via upload

--- MolecularIntegrals.py
+++ MolecularIntegrals.py
@@ -2,7 +2,6 @@ import math
 import numpy as np
 import scipy.misc as scm
 import scipy.special as scs
-from numba import jit
 
 ##INTEGRAL FUNCTIONS
 def Overlap(a, b, la, lb, Ax, Bx):
@@ -73,94 +72,67 @@ def Kin(a, b, Ax, Ay, Az, Bx, By, Bz, la, lb, ma, mb, na, nb, N1, N2, c1, c2):
     return (Tijx[la, lb]*Sy[ma,mb]*Sz[na,nb]+Tijy[ma, mb]*Sx[la,lb]*Sz[na,nb]+Tijz[na, nb]*Sy[ma,mb]*Sx[la,lb])*N, Sx[la, lb]*Sy[ma, mb]*Sz[na, nb]*N
 
 
-def elnuc(a1, a2, Ax, Ay, Az, Bx, By, Bz, l1, l2, m1, m2, n1, n2, N1, N2, c1, c2, input):
-    #SUPER UGLY
+def elnuc(a, b, Ax, Ay, Az, Bx, By, Bz, l1, l2, m1, m2, n1, n2, N1, N2, c1, c2, input):
+    #McMurchie-Davidson scheme    
     N = N1*N2*c1*c2
-    gp = a1 + a2
-    ep = (a1*a2)/gp
+    p = a + b
     
-    Px = (a1*Ax+a2*Bx)/gp
-    Py = (a1*Ay+a2*By)/gp
-    Pz = (a1*Az+a2*Bz)/gp
+    A = np.array([Ax, Ay, Az])
+    B = np.array([Bx, By, Bz])
+    P = gaussian_product_center(a,A,b,B)
     
-    Fzeta = 0
+    
+    val = 0
     for C in range(1, len(input)):
         Zc = input[C][0]
-        rcx = input[C][1]
-        rcy = input[C][2]
-        rcz = input[C][3]
+        C = np.array([input[C][1],input[C][2],input[C][3]])
+        RPC = np.linalg.norm(P-C)
         
-        for i1 in range(0, int(l1/2)+1):
-            for i2 in range(0, int(l2/2)+1):
-                for o1 in range(0, l1-2*i1+1):
-                    for o2 in range(0,l2-2*i2+1):
-                        for r in range(0, int((o1+o2)/2)+1):
-                            ux = l1+l2-2*(i1+i2)-(o1+o2)
-                            for u in range(0, int(ux/2)+1):
-                                for j1 in range(0, int(m1/2)+1):
-                                    for j2 in range(0, int(m2/2)+1):
-                                        for p1 in range(0, m1-2*j1+1):
-                                            for p2 in range(0, m2-2*j2+1):
-                                                for s in range(0, int((p1+p2)/2)+1):
-                                                    uy = m1+m2-2*(j1+j2)-(p1+p2)
-                                                    for v in range(0, int(uy/2)+1):
-                                                        for k1 in range(0, int(n1/2)+1):
-                                                            for k2 in range(0, int(n2/2)+1):
-                                                                for q1 in range(0, n1-2*k1+1):
-                                                                    for q2 in range(0, n2-2*k2+1):
-                                                                        for t in range(0, int((q1+q2)/2)+1):
-                                                                            uz = n1+n2-2*(k1+k2)-(q1+q2)
-                                                                            for w in range(0, int(uz/2)+1):
+        for t in range(0, l1+l2+1):
+            for u in range(0, m1+m2+1):
+                for v in range(0, n1+n2+1):
+                    Ex = E(l1,l2,t,A[0]-B[0],a,b,P[0]-A[0],P[0]-B[0],A[0]-B[0])
+                    Ey = E(m1,m2,u,A[1]-B[1],a,b,P[1]-A[1],P[1]-B[1],A[1]-B[1])
+                    Ez = E(n1,n2,v,A[2]-B[2],a,b,P[2]-A[2],P[2]-B[2],A[2]-B[2])
+                    val += Ex*Ey*Ez*R(t,u,v,0,p,P[0]-C[0],P[1]-C[1],P[2]-C[2],RPC)*Zc
 
-                                                                                Fzeta += A(l1, l2, a1, a2, Ax, Bx, Px, rcx, i1, i2, o1, o2, r, u)*A(m1, m2, a1, a2, Ay, By, Py, rcy, j1, j2, p1, p2, s, v)*A(n1, n2, a1, a2, Az, Bz, Pz, rcz, k1, k2, q1, q2, t, w)*2*F(gp*((Px-rcx)**2+(Py-rcy)**2+(Pz-rcz)**2), ux+uy+uz-(u+v+w)) * Zc
-            
-    Vn = -N*math.pi/gp * math.exp(-ep*((Ax-Bx)**2+(Ay-By)**2+(Az-Bz)**2)) * Fzeta
-
-    return Vn
+    return -val*2*np.pi/p*N
 
 
     
-def elelrep(a1, a2, a3, a4, Ax, Ay, Az, Bx, By, Bz, Cx, Cy, Cz, Dx, Dy, Dz, la, lb, lc, ld, ma, mb, mc, md, na, nb, nc, nd, N1, N2, N3, N4, c1, c2, c3, c4):
-    #SUPER UGLY
+def elelrep(a, b, c, d, Ax, Ay, Az, Bx, By, Bz, Cx, Cy, Cz, Dx, Dy, Dz, l1, l2, l3, l4, m1, m2, m3, m4, n1, n2, n3, n4, N1, N2, N3, N4, c1, c2, c3, c4):
+    #McMurchie-Davidson scheme   
     N = N1*N2*N3*N4*c1*c2*c3*c4
+    A = np.array([Ax, Ay, Az])
+    B = np.array([Bx, By, Bz])
+    C = np.array([Cx, Cy, Cz])
+    D = np.array([Dx, Dy, Dz])
     
-    gp = a1 + a2
-    gq = a3 + a4
-    
-    Px = (a1*Ax+a2*Bx)/gp
-    Py = (a1*Ay+a2*By)/gp
-    Pz = (a1*Az+a2*Bz)/gp
-    
-    Qx = (a3*Cx+a4*Dx)/gq
-    Qy = (a3*Cy+a4*Dy)/gq
-    Qz = (a3*Cz+a4*Dz)/gq
-    
-    K1 = math.exp(-a1*a2*((Ax-Bx)**2 +(Ay-By)**2 +(Az-Bz)**2)/gp)
-    K2 = math.exp(-a3*a4*((Cx-Dx)**2 +(Cy-Dy)**2 +(Cz-Dz)**2)/gq)
-    
-    Fzeta = 0
+    p = a+b
+    q = c+d
+    alpha = p*q/(p+q)
+    P = gaussian_product_center(a,A,b,B)
+    Q = gaussian_product_center(c,C,d,D)
+    RPQ = np.linalg.norm(P-Q)
 
-    for lp in range(0, la+lb+1):
-        for lq in range(0, lc+ld+1):
-            for u1 in range(0, int(lp/2) +1):
-                for u2 in range(0, int(lq/2) +1):
-                    for tp in range(0, int((lp+lq-2*u1-2*u2)/2) +1):
-                        for mp in range(0, ma+mb+1):
-                            for mq in range(0, mc+md+1):
-                                for v1 in range(0, int(mp/2) +1):
-                                    for v2 in range(0, int(mq/2) +1):
-                                        for tpp in range(0, int((mp+mq-2*v1-2*v2)/2) +1):
-                                            for np in range(0, na+nb+1):
-                                                for nq in range(0, nc+nd+1):
-                                                    for om1 in range(0, int(np/2) +1):
-                                                        for om2 in range(0, int(nq/2) +1):
-                                                            for tppp in range(0, int((np+nq-2*om1-2*om2)/2) +1):
-                                                                Fzeta += F(((((Px-Qx)**2+(Py-Qy)**2+(Pz-Qz)**2)*gp*gq)/(gp+gq)),(lp+lq+np+nq+mp+mq-2*u1-2*u2-2*v1-2*v2-2*om1-2*om2-tp-tpp-tppp)) * G(lp, lq, u1, u2, tp, gp, gq, la, lb, (Px-Ax), (Px-Bx), lc, ld, (Qx-Cx), (Qx-Dx), (Px-Qx)) * G(mp, mq, v1, v2, tpp, gp, gq, ma, mb, (Py-Ay), (Py-By), mc, md, (Qy-Cy), (Qy-Dy), (Py-Qy)) * G(np, nq, om1, om2, tppp, gp, gq, na, nb, (Pz-Az), (Pz-Bz), nc, nd, (Qz-Cz), (Qz-Dz), (Pz-Qz))
+    val = 0.0
+    for t in range(l1+l2+1):
+        for u in range(m1+m2+1):
+            for v in range(n1+n2+1):
+                for tau in range(l3+l4+1):
+                    for nu in range(m3+m4+1):
+                        for phi in range(n3+n4+1):
+                            E1 = E(l1,l2,t,A[0]-B[0],a,b,P[0]-A[0],P[0]-B[0],A[0]-B[0])
+                            E2 = E(m1,m2,u,A[1]-B[1],a,b,P[1]-A[1],P[1]-B[1],A[1]-B[1])
+                            E3 = E(n1,n2,v,A[2]-B[2],a,b,P[2]-A[2],P[2]-B[2],A[2]-B[2])
+                            E4 = E(l3,l4,tau,C[0]-D[0],c,d,Q[0]-C[0],Q[0]-D[0],C[0]-D[0])
+                            E5 = E(m3,m4,nu ,C[1]-D[1],c,d,Q[1]-C[1],Q[1]-D[1],C[1]-D[1])
+                            E6 = E(n3,n4,phi,C[2]-D[2],c,d,Q[2]-C[2],Q[2]-D[2],C[2]-D[2])
+                            R1 = R(t+tau,u+nu,v+phi,0,alpha,P[0]-Q[0],P[1]-Q[1],P[2]-Q[2],RPQ) 
+                            val += E1*E2*E3*E4*E5*E6*np.power(-1,tau+nu+phi)*R1
 
-    
-    V = N*((2*math.pi**(5/2)*K1*K2)/(gp*gq*math.sqrt(gp+gq)))*Fzeta
-
-    return V
+    val *= 2*np.power(np.pi,2.5)/(p*q*np.sqrt(p+q)) 
+    return val*N
 
 
 def nucrep(input):
@@ -204,116 +176,88 @@ def u_ObaraSaika(a1, a2, Ax, Ay, Az, Bx, By, Bz, la, lb, ma, mb, na, nb, N1, N2,
     ux[0][0][0] = ux000
     uy[0][0][0] = uy000
     uz[0][0][0] = uz000
-    
     for i in range(0, la+1):
         for j in range(0, lb+1):
             for e in range(0, 2):
                 ux[i+1][j][e] = (Px-Ax)*ux[i][j][e] + 1/(2*p) * (i*ux[i-1][j][e]+j*ux[i][j-1][e]+e*ux[i][j][e-1])
                 ux[i][j+1][e] = (Px-Bx)*ux[i][j][e] + 1/(2*p) * (i*ux[i-1][j][e]+j*ux[i][j-1][e]+e*ux[i][j][e-1])
                 ux[i][j][e+1] = (Px-Cx)*ux[i][j][e] + 1/(2*p) * (i*ux[i-1][j][e]+j*ux[i][j-1][e]+e*ux[i][j][e-1])
-    
     for i in range(0, ma+1):
         for j in range(0, mb+1):
             for e in range(0, 2):
                 uy[i+1][j][e] = (Py-Ay)*uy[i][j][e] + 1/(2*p) * (i*uy[i-1][j][e]+j*uy[i][j-1][e]+e*uy[i][j][e-1])
                 uy[i][j+1][e] = (Py-By)*uy[i][j][e] + 1/(2*p) * (i*uy[i-1][j][e]+j*uy[i][j-1][e]+e*uy[i][j][e-1])
                 uy[i][j][e+1] = (Py-Cy)*uy[i][j][e] + 1/(2*p) * (i*uy[i-1][j][e]+j*uy[i][j-1][e]+e*uy[i][j][e-1])
-                
     for i in range(0, na+1):
         for j in range(0, nb+1):
             for e in range(0, 2):
                 uz[i+1][j][e] = (Pz-Az)*uz[i][j][e] + 1/(2*p) * (i*uz[i-1][j][e]+j*uz[i][j-1][e]+e*uz[i][j][e-1])
                 uz[i][j+1][e] = (Pz-Bz)*uz[i][j][e] + 1/(2*p) * (i*uz[i-1][j][e]+j*uz[i][j-1][e]+e*uz[i][j][e-1])
                 uz[i][j][e+1] = (Pz-Cz)*uz[i][j][e] + 1/(2*p) * (i*uz[i-1][j][e]+j*uz[i][j-1][e]+e*uz[i][j][e-1])
-    
     return -N*ux[la][lb][1]*uy[ma][mb][0]*uz[na][nb][0], -N*ux[la][lb][0]*uy[ma][mb][1]*uz[na][nb][0], -N*ux[la][lb][0]*uy[ma][mb][0]*uz[na][nb][1]
 
-def Velesp(a1, a2, Ax, Ay, Az, Bx, By, Bz, l1, l2, m1, m2, n1, n2, N1, N2, c1, c2, rcx, rcy, rcz):
-    #SUPER UGLY
+def Velesp(a, b, Ax, Ay, Az, Bx, By, Bz, l1, l2, m1, m2, n1, n2, N1, N2, c1, c2, rcx, rcy, rcz):
+    #McMurchie-Davidson scheme    
     N = N1*N2*c1*c2
-    gp = a1 + a2
-    ep = (a1*a2)/gp
-    
-    Px = (a1*Ax+a2*Bx)/gp
-    Py = (a1*Ay+a2*By)/gp
-    Pz = (a1*Az+a2*Bz)/gp
-    
-    Fzeta = 0
-        
-    for i1 in range(0, int(l1/2)+1):
-        for i2 in range(0, int(l2/2)+1):
-            for o1 in range(0, l1-2*i1+1):
-                for o2 in range(0,l2-2*i2+1):
-                    for r in range(0, int((o1+o2)/2)+1):
-                        ux = l1+l2-2*(i1+i2)-(o1+o2)
-                        for u in range(0, int(ux/2)+1):
-                            for j1 in range(0, int(m1/2)+1):
-                                for j2 in range(0, int(m2/2)+1):
-                                    for p1 in range(0, m1-2*j1+1):
-                                        for p2 in range(0, m2-2*j2+1):
-                                            for s in range(0, int((p1+p2)/2)+1):
-                                                uy = m1+m2-2*(j1+j2)-(p1+p2)
-                                                for v in range(0, int(uy/2)+1):
-                                                    for k1 in range(0, int(n1/2)+1):
-                                                        for k2 in range(0, int(n2/2)+1):
-                                                            for q1 in range(0, n1-2*k1+1):
-                                                                for q2 in range(0, n2-2*k2+1):
-                                                                    for t in range(0, int((q1+q2)/2)+1):
-                                                                        uz = n1+n2-2*(k1+k2)-(q1+q2)
-                                                                        for w in range(0, int(uz/2)+1):
-
-                                                                            Fzeta += A(l1, l2, a1, a2, Ax, Bx, Px, rcx, i1, i2, o1, o2, r, u)*A(m1, m2, a1, a2, Ay, By, Py, rcy, j1, j2, p1, p2, s, v)*A(n1, n2, a1, a2, Az, Bz, Pz, rcz, k1, k2, q1, q2, t, w)*2*F(gp*((Px-rcx)**2+(Py-rcy)**2+(Pz-rcz)**2), ux+uy+uz-(u+v+w))
-            
-    Vesp = N*math.pi/gp * math.exp(-ep*((Ax-Bx)**2+(Ay-By)**2+(Az-Bz)**2)) * Fzeta
-    return Vesp
+    p = a + b
+    A = np.array([Ax, Ay, Az])
+    B = np.array([Bx, By, Bz])
+    C = np.array([rcx, rcy, rcz])
+    P = gaussian_product_center(a,A,b,B)
+    RPC = np.linalg.norm(P-C)
+    val = 0
+    for t in range(0, l1+l2+1):
+        for u in range(0, m1+m2+1):
+            for v in range(0, n1+n2+1):
+                Ex = E(l1,l2,t,A[0]-B[0],a,b,P[0]-A[0],P[0]-B[0],A[0]-B[0])
+                Ey = E(m1,m2,u,A[1]-B[1],a,b,P[1]-A[1],P[1]-B[1],A[1]-B[1])
+                Ez = E(n1,n2,v,A[2]-B[2],a,b,P[2]-A[2],P[2]-B[2],A[2]-B[2])
+                val += Ex*Ey*Ez*R(t,u,v,0,p,P[0]-C[0],P[1]-C[1],P[2]-C[2],RPC)
+    return val*2*np.pi/p*N
     
 ##UTILITY FUNCTIONS
-def f(l1, l2, PAc, PBc, k):
-    f = 0
-    for q in range(max(-k, k-2*l2), min(k, 2*l1-k)+1, 2):
-        f += math.factorial(l1)/(math.factorial((k+q)/(2))*math.factorial(l1-(k+q)/(2)))*math.factorial(l2)/(math.factorial((k-q)/(2))*math.factorial(l2-(k-q)/(2)))*PAc**(l1-(k+q)/(2))*PBc**(l2-(k-q)/(2))
-    return f
-
-def F(u, v):
-    # Return wrong values at T very close at zero, why u is defined to be nearly 0
-    if u >= 0 and u < 0.0000001:
-        F = 1/(2*v+1)
+def E(i,j,t,Qx,a,b,XPA,XPB,XAB):
+    #McMurchie-Davidson scheme, 9.5.6 and 9.5.7 Helgaker
+    p = a + b
+    q = a*b/p
+    if (t < 0) or (t > (i + j)):
+        return 0.0
+    elif i == j == t == 0:
+        return np.exp(-q*Qx*Qx)
+    elif j == 0:
+        return (1/(2*p))*E(i-1,j,t-1,Qx,a,b,XPA,XPB,XAB) + XPA*E(i-1,j,t,Qx,a,b,XPA,XPB,XAB) + (t+1)*E(i-1,j,t+1,Qx,a,b,XPA,XPB,XAB)
     else:
-        part1 = math.sqrt(math.pi)/(4**v * u**(v+1/2)) * math.erf(math.sqrt(u))
-        part2 = 0 
-        for k in range(0, v):
-            part2 += math.factorial(v-k)/(4**k * math.factorial(2*v-2*k)*u**(k+1))
-        F = math.factorial(2*v)/(2*math.factorial(v)) * (part1 - math.exp(-u)*part2)
-    return F
+        return (1/(2*p))*E(i,j-1,t-1,Qx,a,b,XPA,XPB,XAB) + XPB*E(i,j-1,t,Qx,a,b,XPA,XPB,XAB) + (t+1)*E(i,j-1,t+1,Qx,a,b,XPA,XPB,XAB)
 
+def R(t,u,v,n,p,PCx,PCy,PCz,RPC):
+    #McMurchie-Davidson scheme, 9.9.18, 9.9.19 and 9.9.20 Helgaker
+    T = p*RPC*RPC
+    val = 0.0
+    if t == u == v == 0:
+        val += np.power(-2*p,n)*boys(n,T)
+    elif t == u == 0:
+        if v > 1:
+            val += (v-1)*R(t,u,v-2,n+1,p,PCx,PCy,PCz,RPC)  
+        val += PCz*R(t,u,v-1,n+1,p,PCx,PCy,PCz,RPC)
+    elif t == 0:
+        if u > 1:
+            val += (u-1)*R(t,u-2,v,n+1,p,PCx,PCy,PCz,RPC)  
+        val += PCy*R(t,u-1,v,n+1,p,PCx,PCy,PCz,RPC)
+    else:
+        if t > 1:
+            val += (t-1)*R(t-2,u,v,n+1,p,PCx,PCy,PCz,RPC)  
+        val += PCx*R(t-1,u,v,n+1,p,PCx,PCy,PCz,RPC)
+    return val
 
-def G(lp, lq, u1, u2, tp, gp, gq, la, lb, PAx, PBx, lc, ld, QCx, QDx, PQx):
-    part1 = (-1)**(lp+tp) * f(la, lb, PAx, PBx, lp)*f(lc, ld, QCx, QDx, lq)
-    part2 = PQx**(lp+lq-2*u1-2*u2-2*tp) * ((gp*gq)/(gp+gq))**(lp+lq-2*u1-2*u2-tp)
-    part3 = gp**(u1-lp) * gq**(u2-lq) * math.factorial(lp) * math.factorial(lq) * math.factorial(lp+lq-2*u1-2*u2) * 4**(-u1-u2-tp)
-    part4 = math.factorial(u1) * math.factorial(u2) * math.factorial(lp-2*u1) * math.factorial(lq-2*u2) * math.factorial(lp+lq-2*u1-2*u2-2*tp) * math.factorial(tp)
-    G = part1*part2*(part3/part4)
-    return G
+def boys(m,T):
+    #Boys functions
+    if abs(T) < 1e-12:
+        return 1/(2*m + 1)
+    else:
+        return scs.gammainc(m+0.5,T)*scs.gamma(m+0.5)/(2*np.power(T,m+0.5))
 
-def I(l1, l2, PAc, PBc, g):
-    I = 0
-    for i in range(0, (int((l1+l2)/2) +1)):
-        I += f(l1, l2, PAc, PBc, 2*i) * (scm.factorial2(2*i-1)/(2*g)**i)*math.sqrt(math.pi/g)
-    return I
-
-def A(l1, l2, a1, a2, Ax, Bx, Px, rcx, i1, i2, o1, o2, r, u):
-    gp = a1 + a2
-    A = 0
-    uc = l1 + l2 - 2*(i1+i2)-(o1+o2)
-    part1 = (-1)**(l1+l2)*math.factorial(l1)*math.factorial(l2)
-    part2 = (-1)**(o2+r)*math.factorial(o1+o2)
-    part3 = 4**(i1+i2+r)*math.factorial(i1)*math.factorial(i2)*math.factorial(o1)*math.factorial(o2)*math.factorial(r)
-    part4 = a1**(o2-i1-r)*a2**(o1-i2-r)*(Ax-Bx)**(o1+o2-2*r)
-    part5 = math.factorial(l1-2*i1-o1)*math.factorial(l2-2*i2-o2)*math.factorial(o1+o2-2*r)
-    part6 = (-1)**(u)*math.factorial(uc)*(Px-rcx)**(uc-2*u)
-    part7 = 4**u*math.factorial(u)*math.factorial(uc-2*u)*gp**(o1+o2-r+u)
-    A = part1*((part2)/(part3))*((part4)/(part5))*((part6)/(part7))
-    return A
+def gaussian_product_center(a,A,b,B):
+    return (a*A+b*B)/(a+b)
 
 ##CALC OF INTEGRALS
 def runIntegrals(input, basis):
@@ -455,13 +399,3 @@ def runQMESP(basis, input, rcx, rcy ,rcz):
             Ve[l,k] = calc
         
     return Ve
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
