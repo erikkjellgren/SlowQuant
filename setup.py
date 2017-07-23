0287b82583e0218645aca82cb4b404d5a522621b
Melisius <erikkjellgren@live.dk>
2017-07-23 21:47:41 +0200

    Fix python34

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup
 from setuptools.extension import Extension
 from Cython.Build import cythonize
 import numpy
+import scipy
 
 my_integrals = [Extension('slowquant.molecularintegrals.MIcython',['slowquant/molecularintegrals/MIcython.pyx'])]
 
-setup(ext_modules=cythonize(my_integrals), include_dirs=[numpy.get_include()])
+setup(ext_modules=cythonize(my_integrals), include_dirs=[numpy.get_include(), scipy.get_include()])
