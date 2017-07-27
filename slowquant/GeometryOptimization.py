Fixed bug in derivatives

slowquant/GeometryOptimization.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 from slowquant import runMolecularIntegrals as MI
 from slowquant import HartreeFock as HF
 from slowquant import BasisSet as BS
@@ -88,13 +89,21 @@ def run_analytic(input, set, results):
             dY[j] = dyHcore + dyERI - dyS + dyenuc[0]
             dZ[j] = dzHcore + dzERI - dzS + dzenuc[0]
         
-        
         for j in range(1, len(dX)):
             input[j,1] = input[j,1] - stepsize*dX[j]
             input[j,2] = input[j,2] - stepsize*dY[j]
             input[j,3] = input[j,3] - stepsize*dZ[j]
         
         output = open('out.txt', 'a')
+        for j in range(1, len(dX)):
+                output.write("{: 12.8e}".format(dX[j]))
+                output.write("\t \t")
+                output.write("{: 12.8e}".format(dY[j]))
+                output.write("\t \t")
+                output.write("{: 12.8e}".format(dZ[j]))
+                output.write('\n')
+        output.write('\n \n')
+        
         for j in range(1, len(input)):
             for k in range(0, 4):
                 output.write("{: 12.8e}".format(input[j,k]))
@@ -114,6 +123,7 @@ def run_numeric(input, set, results):
     GeoOptol = float(set['Geometry Tolerance'])
     stepsize = float(set['Gradient Descent Step'])
     for i in range(1, maxstep):
+        start = time.time()
         dX = np.zeros(len(input))
         dY = np.zeros(len(input))
         dZ = np.zeros(len(input))
@@ -123,39 +133,39 @@ def run_numeric(input, set, results):
             basis = BS.bassiset(input, set)
             results = MI.runIntegrals(input, basis, set, results)
             input[j,1] -= 10**-6
-            results = HF.HartreeFock(input, set, basis, VNN=results['VNN'], Te=results['Te'], S=results['S'], VeN=results['VNe'], Vee=results['Vee'], results=results)
+            results = HF.HartreeFock(input, set, basis, VNN=results['VNN'], Te=results['Te'], S=results['S'], VeN=results['VNe'], Vee=results['Vee'], results=results, print_SCF='No')
             xplus = results['HFenergy']
             input[j,1] -= 10**-6
             basis = BS.bassiset(input, set)
             results = MI.runIntegrals(input, basis, set, results)
             input[j,1] += 10**-6
-            results = HF.HartreeFock(input, set, basis, VNN=results['VNN'], Te=results['Te'], S=results['S'], VeN=results['VNe'], Vee=results['Vee'], results=results)
+            results = HF.HartreeFock(input, set, basis, VNN=results['VNN'], Te=results['Te'], S=results['S'], VeN=results['VNe'], Vee=results['Vee'], results=results, print_SCF='No')
             xminus = results['HFenergy']
             
             input[j,2] += 10**-6
             basis = BS.bassiset(input, set)
             results = MI.runIntegrals(input, basis, set, results)
             input[j,2] -= 10**-6
-            results = HF.HartreeFock(input, set, basis, VNN=results['VNN'], Te=results['Te'], S=results['S'], VeN=results['VNe'], Vee=results['Vee'], results=results)
+            results = HF.HartreeFock(input, set, basis, VNN=results['VNN'], Te=results['Te'], S=results['S'], VeN=results['VNe'], Vee=results['Vee'], results=results, print_SCF='No')
             yplus = results['HFenergy']
             input[j,2] -= 10**-6
             basis = BS.bassiset(input, set)
             results = MI.runIntegrals(input, basis, set, results)
             input[j,2] += 10**-6
-            results = HF.HartreeFock(input, set, basis, VNN=results['VNN'], Te=results['Te'], S=results['S'], VeN=results['VNe'], Vee=results['Vee'], results=results)
+            results = HF.HartreeFock(input, set, basis, VNN=results['VNN'], Te=results['Te'], S=results['S'], VeN=results['VNe'], Vee=results['Vee'], results=results, print_SCF='No')
             yminus = results['HFenergy']
             
             input[j,3] += 10**-6
             basis = BS.bassiset(input, set)
             results = MI.runIntegrals(input, basis, set, results)
             input[j,3] -= 10**-6
-            results = HF.HartreeFock(input, set, basis, VNN=results['VNN'], Te=results['Te'], S=results['S'], VeN=results['VNe'], Vee=results['Vee'], results=results)
+            results = HF.HartreeFock(input, set, basis, VNN=results['VNN'], Te=results['Te'], S=results['S'], VeN=results['VNe'], Vee=results['Vee'], results=results, print_SCF='No')
             zplus = results['HFenergy']
             input[j,3] -= 10**-6
             basis = BS.bassiset(input, set)
             results = MI.runIntegrals(input, basis, set, results)
             input[j,3] += 10**-6
-            results = HF.HartreeFock(input, set, basis, VNN=results['VNN'], Te=results['Te'], S=results['S'], VeN=results['VNe'], Vee=results['Vee'], results=results)
+            results = HF.HartreeFock(input, set, basis, VNN=results['VNN'], Te=results['Te'], S=results['S'], VeN=results['VNe'], Vee=results['Vee'], results=results, print_SCF='No')
             zminus = results['HFenergy']
             
             dX[j] = (xplus-xminus)/(2*10**-6)
@@ -166,6 +176,13 @@ def run_numeric(input, set, results):
             input[j,1] = input[j,1] - stepsize*dX[j]
             input[j,2] = input[j,2] - stepsize*dY[j]
             input[j,3] = input[j,3] - stepsize*dZ[j]
+            
+        print(time.time()-start, 'GeoOpt step '+str(i))
+        
+        # Get energy of new structure
+        basis = BS.bassiset(input, set)
+        results = MI.runIntegrals(input, basis, set, results)
+        results = HF.HartreeFock(input, set, basis, VNN=results['VNN'], Te=results['Te'], S=results['S'], VeN=results['VNe'], Vee=results['Vee'], results=results)
         
         output = open('out.txt', 'a')
         for j in range(1, len(dX)):