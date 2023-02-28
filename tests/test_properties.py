d3528c134b2c2c0f82abfdcfe2f98cf0b62210e2
Erik Kjellgren <erikkjellgren@live.dk>
2023-02-28 12:32:58 +0100

    Fixed wrong tests

--- tests/test_properties.py
+++ tests/test_properties.py
@@ -89,15 +89,14 @@ def test_properties_ch4_sto3g() -> None:
 def test_properties_ch3_sto3g() -> None:
     """Test unrestricted Hartree-Fock for CH3 with STO-3G.
 
-    Does NOT give the same as in the reference. (Reference is: 0.0152)
     Reference data: Szabo and Ostlund
     """
     A = sq.SlowQuant()
     A.set_molecule(
         """C   0.000000000000  0.000000000000   0.000000000000;
            H   2.039000000000  0.000000000000   0.000000000000;
-           H  -1.095000000000  1.765825798316   0.000000000000;
-           H  -1.095000000000 -1.765825798316   0.000000000000""",
+           H  -1.019500000000  1.765825798316   0.000000000000;
+           H  -1.019500000000 -1.765825798316   0.000000000000""",
         distance_unit="bohr",
     )
     A.set_basis_set("sto-3g")
@@ -106,21 +105,20 @@ def test_properties_ch3_sto3g() -> None:
     A.hartree_fock.rmsd_threshold = 10**-6
     A.hartree_fock.run_unrestricted_hartree_fock()
     A.init_properties()
-    assert abs(A.hartree_fock.spin_contamination - 0.016189571516672885) < 10**-4
+    assert abs(A.hartree_fock.spin_contamination - 0.0152) < 10**-4
 
 
 def test_properties_ch3_431g() -> None:
     """Test unrestricted Hartree-Fock for CH3 with 4-31G.
 
-    Does NOT give the same as in the reference. (Reference is: 0.0122)
     Reference data: Szabo and Ostlund
     """
     A = sq.SlowQuant()
     A.set_molecule(
         """C   0.000000000000  0.000000000000   0.000000000000;
            H   2.039000000000  0.000000000000   0.000000000000;
-           H  -1.095000000000  1.765825798316   0.000000000000;
-           H  -1.095000000000 -1.765825798316   0.000000000000""",
+           H  -1.019500000000  1.765825798316   0.000000000000;
+           H  -1.019500000000 -1.765825798316   0.000000000000""",
         distance_unit="bohr",
     )
     A.set_basis_set("4-31g")
@@ -129,44 +127,43 @@ def test_properties_ch3_431g() -> None:
     A.hartree_fock.rmsd_threshold = 10**-6
     A.hartree_fock.run_unrestricted_hartree_fock()
     A.init_properties()
-    assert abs(A.hartree_fock.spin_contamination - 0.013124715896593031) < 10**-4
+    assert abs(A.hartree_fock.spin_contamination - 0.0122) < 10**-4
 
 
 def test_properties_ch3_631gs() -> None:
     """Test unrestricted Hartree-Fock for CH3 with 6-31G*.
 
-    Does NOT give the same as in the reference. (Reference is: 0.0118)
     Reference data: Szabo and Ostlund
     """
     A = sq.SlowQuant()
     A.set_molecule(
         """C   0.000000000000  0.000000000000   0.000000000000;
            H   2.039000000000  0.000000000000   0.000000000000;
-           H  -1.095000000000  1.765825798316   0.000000000000;
-           H  -1.095000000000 -1.765825798316   0.000000000000""",
+           H  -1.019500000000  1.765825798316   0.000000000000;
+           H  -1.019500000000 -1.765825798316   0.000000000000""",
         distance_unit="bohr",
     )
     A.set_basis_set("6-31g*")
     A.init_hartree_fock()
-    A.hartree_fock.de_threshold = 10**-6
-    A.hartree_fock.rmsd_threshold = 10**-6
+    A.hartree_fock.de_threshold = 10**-7
+    A.hartree_fock.rmsd_threshold = 10**-7
+    A.hartree_fock.max_scf_iterations = 200
     A.hartree_fock.run_unrestricted_hartree_fock()
     A.init_properties()
-    assert abs(A.hartree_fock.spin_contamination - 0.012599564332392621) < 10**-4
+    assert abs(A.hartree_fock.spin_contamination - 0.0118) < 10**-4
 
 
 def test_properties_ch3_631gss() -> None:
     """Test unrestricted Hartree-Fock for CH3 with 6-31G**.
 
-    Does NOT give the same as in the reference. (Reference is: 0.0114)
     Reference data: Szabo and Ostlund
     """
     A = sq.SlowQuant()
     A.set_molecule(
         """C   0.000000000000  0.000000000000   0.000000000000;
            H   2.039000000000  0.000000000000   0.000000000000;
-           H  -1.095000000000  1.765825798316   0.000000000000;
-           H  -1.095000000000 -1.765825798316   0.000000000000""",
+           H  -1.019500000000  1.765825798316   0.000000000000;
+           H  -1.019500000000 -1.765825798316   0.000000000000""",
         distance_unit="bohr",
     )
     A.set_basis_set("6-31g**")
@@ -175,4 +172,4 @@ def test_properties_ch3_631gss() -> None:
     A.hartree_fock.rmsd_threshold = 10**-6
     A.hartree_fock.run_unrestricted_hartree_fock()
     A.init_properties()
-    assert abs(A.hartree_fock.spin_contamination - 0.01215019463895084) < 10**-4
+    assert abs(A.hartree_fock.spin_contamination - 0.0114) < 10**-4
