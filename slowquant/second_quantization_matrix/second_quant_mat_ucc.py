536835b9c221c817b5ebf1d60a2afd0649f0d6bb
Erik Kjellgren <erikkjellgren@live.dk>
2023-03-27 17:45:19 +0200

    UCCS

--- slowquant/second_quantization_matrix/second_quant_mat_ucc.py
+++ slowquant/second_quantization_matrix/second_quant_mat_ucc.py
@@ -1,7 +1,7 @@
 import numpy as np
 import scipy
 import scipy.optimize
-from slowquant.second_quantization_matrix.second_quant_mat_base import H, kronecker_product
+from slowquant.second_quantization_matrix.second_quant_mat_base import H, kronecker_product, a_op_spin
 from functools import partial
 import time
 from scipy.sparse import csr_matrix
@@ -103,6 +103,65 @@ class WaveFunctionUCC:
         self.hf_energy = res["fun"]
         self.kappa = res["x"]
 
+    def run_UCCS(self, orbital_optimization: bool = False) -> None:
+        if orbital_optimization:
+            e_tot = partial(
+                total_energy_UCC,
+                num_spin_orbs=self.num_spin_orbs,
+                num_elec=self.num_elec,
+                on_vector=self.on_vector,
+                c_orthonormal=self.c_orthonormal,
+                h_core=self.h_core,
+                g_eri=self.g_eri,
+                active_occ=self.active_occ,
+                active_unocc=self.active_unocc,
+                excitations="S",
+                orbital_optimized =  True,
+                kappa_idx=self.kappa_idx
+            )
+        else:
+            e_tot = partial(
+                total_energy_UCC,
+                num_spin_orbs=self.num_spin_orbs,
+                num_elec=self.num_elec,
+                on_vector=self.on_vector,
+                c_orthonormal=self.c_orthonormal,
+                h_core=self.h_core,
+                g_eri=self.g_eri,
+                active_occ=self.active_occ,
+                active_unocc=self.active_unocc,
+                excitations="S",
+                orbital_optimized = False,
+                kappa_idx = []
+            )
+        global iteration
+        global start
+        iteration = 0
+        start = time.time()
+
+        def print_progress(X: list[float]) -> None:
+            global iteration
+            global start
+            print(iteration, time.time() - start, e_tot(X))
+            iteration += 1
+            start = time.time()
+
+        if orbital_optimization:
+            res = scipy.optimize.minimize(e_tot, self.kappa+self.theta1, tol=1e-6, callback=print_progress)
+            self.uccs_energy = res["fun"]
+            self.kappa = res["x"][:len(self.kappa)]
+            self.theta1 = res["x"][len(self.kappa):]
+        else:
+            res = scipy.optimize.minimize(e_tot, self.theta1, tol=1e-6, callback=print_progress)
+            self.uccs_energy = res["fun"]
+            self.theta1 = res["x"]
+
+    def run_UCCD(self, orbutal_optimization: bool = False) -> None:
+        None
+
+    def run_UCCSD(self, orbutal_optimization: bool = False) -> None:
+        None
+
 
 def total_energy_HF(
     kappa: list[float],
@@ -122,3 +181,85 @@ def total_energy_HF(
     HF_ket = on_vector.transpose()
     HF_bra = np.conj(HF_ket).transpose()
     return HF_bra.dot(H(h_core, g_eri, c_trans, num_spin_orbs, num_elec).dot(HF_ket)).toarray()[0,0]
+
+def total_energy_UCC(
+    parameters: list[float],
+    num_spin_orbs: int,
+    num_elec: int,
+    on_vector: np.ndarray,
+    c_orthonormal: np.ndarray,
+    h_core: np.ndarray,
+    g_eri: np.ndarray,
+    active_occ: list[int],
+    active_unocc: list[int],
+    excitations: str,
+    orbital_optimized: bool,
+    kappa_idx: list[list[int, int]],
+) -> float:
+    kappa = []
+    theta1 = []
+    theta2 = []
+    idx_counter = 0
+    for i in range(len(kappa_idx)):
+        kappa.append(parameters[idx_counter])
+        idx_counter += 1
+    if "S" in excitations:
+        for a in range(num_spin_orbs):
+            for i in range(num_spin_orbs):
+                if i in active_occ and a in active_unocc:
+                    theta1.append(parameters[idx_counter])
+                    idx_counter += 1
+    if "D" in excitations:
+        for a in range(num_spin_orbs):
+            for b in range(num_spin_orbs):
+                if a >= b:
+                    continue
+                for i in range(num_spin_orbs):
+                    for j in range(num_spin_orbs):
+                        if i >= j:
+                            continue
+                        if i in active_occ and j in active_occ and a in active_unocc and b in active_unocc:
+                            theta2.append(parameters[idx_counter])
+                            idx_counter += 1
+
+    if orbital_optimized:
+        kappa_mat = np.zeros_like(c_orthonormal)
+        for kappa_val, (p, q) in zip(kappa, kappa_idx):
+            kappa_mat[p, q] = kappa_val
+            kappa_mat[q, p] = -kappa_val
+        c_trans = np.matmul(c_orthonormal, scipy.linalg.expm(-kappa_mat))
+    else:
+        c_trans = c_orthonormal
+    
+    t = np.zeros((2**num_spin_orbs,2**num_spin_orbs))
+    if "S" in excitations:
+        counter = 0
+        for a in range(num_spin_orbs):
+            for i in range(num_spin_orbs):
+                if i in active_occ and a in active_unocc:
+                    tmp = a_op_spin(a, True, num_spin_orbs, num_elec).matrix_form.dot(a_op_spin(i, False, num_spin_orbs, num_elec).matrix_form)
+                    t += theta1[counter]*tmp
+                    counter += 1
+    
+    if "D" in excitations:
+        counter = 0
+        for a in range(num_spin_orbs):
+            for b in range(num_spin_orbs):
+                if a >= b:
+                    continue
+                for i in range(num_spin_orbs):
+                    for j in range(num_spin_orbs):
+                        if i >= j:
+                            continue
+                        if i in active_occ and j in active_occ and a in active_unocc and b in active_unocc:
+                            tmp = a_op_spin(a, True, num_spin_orbs, num_elec).matrix_form.dot(a_op_spin(b, True, num_spin_orbs, num_elec).matrix_form)
+                            tmp = tmp.dot(a_op_spin(j, False, num_spin_orbs, num_elec).matrix_form)
+                            tmp = tmp.dot(a_op_spin(i, False, num_spin_orbs, num_elec).matrix_form)
+                            t += theta2[counter]*tmp
+                            counter += 1
+
+    T = t - np.conj(t).transpose()
+    U = csr_matrix(scipy.linalg.expm(T))
+    UCC_ket = U.dot(on_vector.transpose())
+    UCC_bra = np.conj(UCC_ket).transpose()
+    return UCC_bra.dot(H(h_core, g_eri, c_trans, num_spin_orbs, num_elec).dot(UCC_ket)).toarray()[0,0]
