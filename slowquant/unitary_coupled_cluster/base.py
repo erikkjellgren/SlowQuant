9a17b421a4cfd24806451c3ac9cac277bd805ffe
Erik Kjellgren <erikkjellgren@live.dk>
2023-04-08 17:00:12 +0200

    Some changes

--- slowquant/unitary_coupled_cluster/base.py
+++ slowquant/unitary_coupled_cluster/base.py
@@ -1,21 +1,40 @@
 from __future__ import annotations
 
+import copy
+import time
+from functools import cache
+
+import numpy as np
+import scipy.sparse as ss
+
 from slowquant.molecularintegrals.integralfunctions import (
     one_electron_integral_transform,
     two_electron_integral_transform,
 )
-import numpy as np
-import copy
-import scipy.sparse as ss 
-from functools import cache
 
 Z_mat = np.array([[1, 0], [0, -1]], dtype=float)
 I_mat = np.array([[1, 0], [0, 1]], dtype=float)
 a_mat = np.array([[0, 1], [0, 0]], dtype=float)
 a_mat_dagger = np.array([[0, 0], [1, 0]], dtype=float)
 
+
 @cache
-def a_op_spin(idx: int, dagger: bool, number_spin_orbitals: int, number_of_electrons: int):
+def a_op_spin_matrix(idx: int, dagger: bool, number_spin_orbitals: int, number_electrons: int) -> np.ndarray:
+    r"""Get matrix representation of fermionic operator.
+    This is the matrix form that depends on number of electrons i.e.:
+
+    .. math::
+        Z x Z x .. x a x .. I x I
+
+    Args:
+        idx: Spin orbital index.
+        dagger: Creation or annihilation operator.
+        number_spin_orbitals: Total number of spin orbitals.
+        number_electrons: Total number of electrons.
+
+    Returns:
+        Matrix representation of ferminonic annihilation or creation operator.
+    """
     operators = []
     for i in range(number_spin_orbitals):
         if i == idx:
@@ -23,17 +42,59 @@ def a_op_spin(idx: int, dagger: bool, number_spin_orbitals: int, number_of_elect
                 operators.append(a_mat_dagger)
             else:
                 operators.append(a_mat)
-        elif i <= number_of_electrons and i < idx:
+        elif i <= number_electrons and i < idx:
             operators.append(Z_mat)
         else:
             operators.append(I_mat)
     return kronecker_product(operators)
 
+
+def a_op_spin(idx: int, dagger: bool, number_spin_orbitals: int, number_electrons: int) -> np.ndarray:
+    operators = []
+    for i in range(number_spin_orbitals):
+        if i == idx:
+            if dagger:
+                operators.append(a_mat_dagger)
+            else:
+                operators.append(a_mat)
+        elif i <= number_electrons and i < idx:
+            operators.append(Z_mat)
+        else:
+            operators.append(I_mat)
+    return operators
+
+
 @cache
-def kronecker_product_cached(num_prior, num_after, val00, val01, val10, val11, is_csr):
-    """Does the P x P x P ..."""
-    if val00 not in [-1,0,1] or val01 not in [-1,0,1] or val10 not in [-1,0,1] or val11 not in [-1,0,1]:
-        print(f"WARNING: Unexpected element values in cahced kronecker product: {val00} {val01} {val10} {val11}")
+def kronecker_product_cached(
+    num_prior: int, num_after: int, val00: int, val01: int, val10: int, val11: int, is_csr: bool
+) -> np.ndarray | ss.csr_matrix:
+    r"""Get operator in matrix form.
+    The operator is returned in the form:
+
+    .. math::
+        I x I x .. o .. x I x I
+
+    Args:
+       num_prior: Number of left-hand side identity matrices.
+       num_after: Number of right-hand side identity matrices.
+       val00: First value of operator matrix.
+       val10: Second value of operator matrix.
+       val01: Third value of operator matrix.
+       val11: Fourth value of operator matrix.
+       is_csr: If the resulting matrix representation should be a sparse matrix.
+
+    Returns:
+       Matrix representation ofi an operator.
+    """
+    if (
+        val00 not in [-1, 0, 1]
+        or val01 not in [-1, 0, 1]
+        or val10 not in [-1, 0, 1]
+        or val11 not in [-1, 0, 1]
+    ):
+        print(
+            f"WARNING: Unexpected element values in cahced kronecker product: {val00} {val01} {val10} {val11}"
+        )
     if is_csr:
         I1 = ss.identity(int(2**num_prior))
         I2 = ss.identity(int(2**num_after))
@@ -47,7 +108,18 @@ def kronecker_product_cached(num_prior, num_after, val00, val01, val10, val11, i
 
 
 def kronecker_product(A: list[np.ndarray]) -> np.ndarray:
-    """Does the P x P x P ..."""
+    r"""Get Kronecker product of a list of matricies
+    Does:
+
+    .. math::
+        P x P x P ...
+
+    Args:
+       A: List of matrices.
+
+    Returns:
+       Kronecker product of matrices.
+    """
     if len(A) < 2:
         return A
     total = np.kron(A[0], A[1])
@@ -57,48 +129,56 @@ def kronecker_product(A: list[np.ndarray]) -> np.ndarray:
 
 
 class StateVector:
-    def __init__(self, inactive, active) -> None:
+    """State vector."""
+
+    def __init__(self, inactive: list(np.ndarray), active: list(np.ndarray)) -> None:
+        """Initialize state vector.
+
+        Args:
+            inactive: Kronecker representation of inactive orbitals (reference).
+            active: Kronecker representation of active orbitals (refernce).
+        """
         self.inactive = np.transpose(inactive)
         self._active_onvector = active
         self._active = np.transpose(kronecker_product(active))
 
     @property
-    def bra_inactive(self):
+    def bra_inactive(self) -> list(np.ndarray):
         return np.conj(self.inactive).transpose()
 
     @property
-    def ket_inactive(self):
+    def ket_inactive(self) -> list(np.ndarray):
         return self.inactive
-    
+
     @property
-    def bra_active(self):
+    def bra_active(self) -> np.ndarray:
         return np.conj(self.active).transpose()
 
     @property
-    def ket_active(self):
+    def ket_active(self) -> np.ndarray:
         return self.active
 
     @property
-    def bra_active_csr(self):
+    def bra_active_csr(self) -> ss.csr_matrix:
         return ss.csr_matrix(np.conj(self.active).transpose())
 
     @property
-    def ket_active_csr(self):
+    def ket_active_csr(self) -> ss.csr_matrix:
         return ss.csr_matrix(self.active).transpose()
 
     @property
-    def U(self):
+    def U(self) -> np.ndarray:
         return self.U_
 
     @U.setter
-    def U(self, u):
+    def U(self, u: np.ndarray) -> None:
         self.active = np.matmul(u, self._active)
         self.U_ = u
 
 
 def a_op(
-        spinless_idx: int, spin: str, dagger: bool, number_spin_orbitals: int, number_of_electrons: int
-    ) -> list[np.ndarray]:
+    spinless_idx: int, spin: str, dagger: bool, number_spin_orbitals: int, number_of_electrons: int
+) -> list[np.ndarray]:
     idx = 2 * spinless_idx
     if spin == "beta":
         idx += 1
@@ -116,55 +196,82 @@ def a_op(
     return operators
 
 
-import time
-
-def expectation_value(bra: StateVector, fermiop: FermionicOperator, ket: StateVector, use_csr: int = 9) -> float:
+def expectation_value(
+    bra: StateVector, fermiop: FermionicOperator, ket: StateVector, use_csr: int = 8
+) -> float:
     if len(bra.inactive) != len(ket.inactive):
-        raise ValueError('Bra and Ket does not have same number of inactive orbitals')
+        raise ValueError("Bra and Ket does not have same number of inactive orbitals")
     if len(bra._active) != len(ket._active):
-        raise ValueError('Bra and Ket does not have same number of active orbitals')
+        raise ValueError("Bra and Ket does not have same number of active orbitals")
     total = 0
     start = time.time()
     for op in fermiop.operators:
         tmp = 1
         for i in range(len(bra.inactive)):
-            tmp *= np.matmul(bra.bra_inactive[i], np.matmul(op[i], ket.ket_inactive[:,i]))
+            tmp *= np.matmul(bra.bra_inactive[i], np.matmul(op[i], ket.ket_inactive[:, i]))
         number_active_orbitals = len(bra._active_onvector)
         active_start = len(bra.inactive)
         active_end = active_start + number_active_orbitals
         if number_active_orbitals != 0:
             if number_active_orbitals >= use_csr:
-                operator = ss.identity(2**number_active_orbitals)
+                operator = ket.ket_active_csr
             else:
-                operator = np.identity(2**number_active_orbitals)
-            for op_element_idx, op_element in enumerate(op[active_start:active_end]):
+                operator = ket.ket_active
+            for op_element_idx, op_element in enumerate(op[active_start:active_end][::-1]):
                 prior = op_element_idx
                 after = number_active_orbitals - op_element_idx - 1
                 factor = 1
-                if abs(op_element[0,0]) not in [0, 1]:
-                    factor *= op_element[0,0]
-                    op_element = op_element/factor
-                elif abs(op_element[0,1]) not in [0, 1]:
-                    factor *= op_element[0,1]
-                    op_element = op_element/factor
-                elif abs(op_element[1,0]) not in [0, 1]:
-                    factor *= op_element[1,0]
-                    op_element = op_element/factor
-                elif abs(op_element[1,1]) not in [0, 1]:
-                    factor *= op_element[1,1]
-                    op_element = op_element/factor
-                if op_element[0,0] == 1 and op_element[0,1] == 0 and op_element[1,0] == 0 and op_element[1,1] == 1 and factor == 1:
+                if abs(op_element[0, 0]) not in [0, 1]:
+                    factor *= op_element[0, 0]
+                    op_element = op_element / factor
+                elif abs(op_element[0, 1]) not in [0, 1]:
+                    factor *= op_element[0, 1]
+                    op_element = op_element / factor
+                elif abs(op_element[1, 0]) not in [0, 1]:
+                    factor *= op_element[1, 0]
+                    op_element = op_element / factor
+                elif abs(op_element[1, 1]) not in [0, 1]:
+                    factor *= op_element[1, 1]
+                    op_element = op_element / factor
+                if (
+                    op_element[0, 0] == 1
+                    and op_element[0, 1] == 0
+                    and op_element[1, 0] == 0
+                    and op_element[1, 1] == 1
+                    and factor == 1
+                ):
                     continue
                 if number_active_orbitals >= use_csr:
-                    operator = factor*operator.dot(kronecker_product_cached(prior, after, op_element[0,0], op_element[0,1], op_element[1,0], op_element[1,1], True))
+                    operator = factor * kronecker_product_cached(
+                        prior,
+                        after,
+                        op_element[0, 0],
+                        op_element[0, 1],
+                        op_element[1, 0],
+                        op_element[1, 1],
+                        True,
+                    ).dot(operator)
+                    if operator.nnz == 0:
+                        break
                 else:
-                    operator = factor*np.matmul(operator, kronecker_product_cached(prior, after, op_element[0,0], op_element[0,1], op_element[1,0], op_element[1,1], False))
-            if number_active_orbitals >= use_csr: 
-                tmp *= bra.bra_active_csr.dot(operator.dot(ket.ket_active_csr)).toarray()[0,0]
+                    operator = factor * np.matmul(
+                        kronecker_product_cached(
+                            prior,
+                            after,
+                            op_element[0, 0],
+                            op_element[0, 1],
+                            op_element[1, 0],
+                            op_element[1, 1],
+                            False,
+                        ),
+                        operator,
+                    )
+            if number_active_orbitals >= use_csr:
+                tmp *= bra.bra_active_csr.dot(operator).toarray()[0, 0]
             else:
-                tmp *= np.matmul(bra.bra_active, np.matmul(operator, ket.ket_active))  
+                tmp *= np.matmul(bra.bra_active, operator)
         total += tmp
-    print(time.time() - start)
+    print(f"Expectation value: {time.time() - start}")
     return total
 
 
@@ -186,27 +293,27 @@ class FermionicOperator:
 
     def __sub__(self, fermiop: FermionicOperator) -> FermionicOperator:
         if self.operators == [[]]:
-            operators_new = (-1*fermiop).operators
+            operators_new = (-1 * fermiop).operators
         elif fermiop.operators == [[]]:
             operators_new = copy.copy(self.operators)
         else:
-            operators_new = self.operators + (-1*fermiop).operators
+            operators_new = self.operators + (-1 * fermiop).operators
         return FermionicOperator(operators_new)
 
     def __mul__(self, fermop: FermionicOperator) -> FermionicOperator:
         operators_new = []
         for op1 in self.operators:
             if op1 == []:
-               continue
+                continue
             for op2 in fermop.operators:
                 if op2 == []:
-                   continue
+                    continue
                 new_op = copy.copy(op1)
                 is_zero = False
                 for i in range(len(new_op)):
                     new_op[i] = np.matmul(new_op[i], op2[i])
-                    if abs(new_op[i][0,0]) < 10**-12 and abs(new_op[i][0,1]) < 10**-12 and abs(new_op[i][1,0]) < 10**-12 and abs(new_op[i][1,1]) < 10**-12:
-                        is_zero = True
+                    # if abs(new_op[i][0,0]) < 10**-12 and abs(new_op[i][0,1]) < 10**-12 and abs(new_op[i][1,0]) < 10**-12 and abs(new_op[i][1,1]) < 10**-12:
+                    #    is_zero = True
                 if not is_zero:
                     operators_new.append(new_op)
         return FermionicOperator(operators_new)
@@ -216,20 +323,34 @@ class FermionicOperator:
         for op in self.operators:
             op_new = copy.copy(op)
             op_new[0] *= number
-            if np.sum(np.abs(op_new[0])) > 10**-12:
+            if True:  # if np.sum(np.abs(op_new[0])) > 10**-12:
                 operators_new.append(op_new)
         if len(operators_new) == 0:
             return FermionicOperator([[]])
         return FermionicOperator(operators_new)
 
+    @property
+    def dagger(self):
+        operators_new = []
+        for op in self.operators:
+            new_op = []
+            for mat in op:
+                new_op.append(np.conj(mat).transpose())
+            operators_new.append(new_op)
+        return FermionicOperator(operators_new)
 
-def Epq(p: int, q: int, num_spin_orbs: int, num_elec: int) -> np.ndarray:
-    E = FermionicOperator(a_op(p, "alpha", True, num_spin_orbs, num_elec)) * FermionicOperator(a_op(q, "alpha", False, num_spin_orbs, num_elec))
-    E += FermionicOperator(a_op(p, "beta", True, num_spin_orbs, num_elec)) * FermionicOperator(a_op(q, "beta", False, num_spin_orbs, num_elec))
+
+def Epq(p: int, q: int, num_spin_orbs: int, num_elec: int) -> FermionicOperator:
+    E = FermionicOperator(a_op(p, "alpha", True, num_spin_orbs, num_elec)) * FermionicOperator(
+        a_op(q, "alpha", False, num_spin_orbs, num_elec)
+    )
+    E += FermionicOperator(a_op(p, "beta", True, num_spin_orbs, num_elec)) * FermionicOperator(
+        a_op(q, "beta", False, num_spin_orbs, num_elec)
+    )
     return E
 
 
-def epqrs(p: int, q: int, r: int, s: int, num_spin_orbs: int, num_elec: int) -> np.ndarray:
+def epqrs(p: int, q: int, r: int, s: int, num_spin_orbs: int, num_elec: int) -> FermionicOperator:
     if q == r:
         return Epq(p, q, num_spin_orbs, num_elec) * Epq(r, s, num_spin_orbs, num_elec) - Epq(
             p, s, num_spin_orbs, num_elec
@@ -237,14 +358,16 @@ def epqrs(p: int, q: int, r: int, s: int, num_spin_orbs: int, num_elec: int) -> 
     return Epq(p, q, num_spin_orbs, num_elec) * Epq(r, s, num_spin_orbs, num_elec)
 
 
-def Eminuspq(p: int, q: int, num_spin_orbs: int, num_elec: int) -> np.ndarray:
+def Eminuspq(p: int, q: int, num_spin_orbs: int, num_elec: int) -> FermionicOperator:
     return Epq(p, q, num_spin_orbs, num_elec) - Epq(q, p, num_spin_orbs, num_elec)
 
 
-def Hamiltonian(h: np.ndarray, g: np.ndarray, c_mo: np.ndarray, num_spin_orbs: int, num_elec: int) -> np.ndarray:
+def Hamiltonian(
+    h: np.ndarray, g: np.ndarray, c_mo: np.ndarray, num_spin_orbs: int, num_elec: int
+) -> FermionicOperator:
     h_mo = one_electron_integral_transform(c_mo, h)
     g_mo = two_electron_integral_transform(c_mo, g)
-    num_spatial_orbs = num_spin_orbs//2
+    num_spatial_orbs = num_spin_orbs // 2
     for p in range(num_spatial_orbs):
         for q in range(num_spatial_orbs):
             if p == 0 and q == 0:
@@ -257,3 +380,7 @@ def Hamiltonian(h: np.ndarray, g: np.ndarray, c_mo: np.ndarray, num_spin_orbs: i
                 for s in range(num_spatial_orbs):
                     H_expectation += 1 / 2 * g_mo[p, q, r, s] * epqrs(p, q, r, s, num_spin_orbs, num_elec)
     return H_expectation
+
+
+def commutator(A: FermionicOperator, B: FermionicOperator) -> FermionicOperator:
+    return A * B - B * A
--- slowquant/unitary_coupled_cluster/util.py
+++ slowquant/unitary_coupled_cluster/util.py
@@ -1,8 +1,12 @@
 import numpy as np
 import scipy.linalg
-from slowquant.unitary_coupled_cluster.base import a_op_spin
 
-def construct_integral_trans_mat(c_orthonormal: np.ndarray, kappa: list[float], kappa_idx: list[list[int]]) -> np.ndarray:
+from slowquant.unitary_coupled_cluster.base import a_op_spin_matrix
+
+
+def construct_integral_trans_mat(
+    c_orthonormal: np.ndarray, kappa: list[float], kappa_idx: list[list[int]]
+) -> np.ndarray:
     kappa_mat = np.zeros_like(c_orthonormal)
     for kappa_val, (p, q) in zip(kappa, kappa_idx):
         kappa_mat[p, q] = kappa_val
@@ -10,22 +14,24 @@ def construct_integral_trans_mat(c_orthonormal: np.ndarray, kappa: list[float], 
     c_trans = np.matmul(c_orthonormal, scipy.linalg.expm(-kappa_mat))
     return c_trans
 
+
 def iterate_T1(active_occ: list[int], active_unocc: list[int]) -> tuple[int]:
     for a in active_unocc:
         for i in active_occ:
             num_alpha = 0
             num_beta = 0
-            if a%2 == 0:
+            if a % 2 == 0:
                 num_alpha += 1
             else:
                 num_beta += 1
-            if i%2 == 0:
+            if i % 2 == 0:
                 num_alpha += 1
             else:
                 num_beta += 1
-            if num_alpha%2 != 0 or num_beta%2 != 0:
-                continue
+            # if num_alpha%2 != 0 or num_beta%2 != 0:
+            #    continue
             yield a, i
+
 
 def iterate_T2(active_occ: list[int], active_unocc: list[int]) -> tuple[int]:
     for a in active_unocc:
@@ -38,44 +44,62 @@ def iterate_T2(active_occ: list[int], active_unocc: list[int]) -> tuple[int]:
                         continue
                     num_alpha = 0
                     num_beta = 0
-                    if a%2 == 0:
+                    if a % 2 == 0:
                         num_alpha += 1
                     else:
                         num_beta += 1
-                    if b%2 == 0:
+                    if b % 2 == 0:
                         num_alpha += 1
                     else:
                         num_beta += 1
-                    if i%2 == 0:
+                    if i % 2 == 0:
                         num_alpha += 1
                     else:
                         num_beta += 1
-                    if j%2 == 0:
+                    if j % 2 == 0:
                         num_alpha += 1
                     else:
                         num_beta += 1
-                    if num_alpha%2 != 0 or num_beta%2 != 0:
-                        continue
+                    # if num_alpha%2 != 0 or num_beta%2 != 0:
+                    #    continue
                     yield a, i, b, j
 
-def construct_UCC_U(num_spin_orbs: int, num_elec: int, theta: list[float], excitations: str, active_occ: list[int], active_unocc: list[int]) -> np.ndarray:
-    t = np.zeros((2**num_spin_orbs,2**num_spin_orbs))
+
+import time
+
+
+def construct_UCC_U(
+    num_spin_orbs: int,
+    num_elec: int,
+    theta: list[float],
+    excitations: str,
+    active_occ: list[int],
+    active_unocc: list[int],
+) -> np.ndarray:
+    t = np.zeros((2**num_spin_orbs, 2**num_spin_orbs))
     counter = 0
     if "s" in excitations:
         for (a, i) in iterate_T1(active_occ, active_unocc):
             if theta[counter] != 0.0:
-                tmp = a_op_spin(a, True, num_spin_orbs, num_elec).dot(a_op_spin(i, False, num_spin_orbs, num_elec))
-                t += theta[counter]*tmp
+                tmp = a_op_spin_matrix(a, True, num_spin_orbs, num_elec).dot(
+                    a_op_spin_matrix(i, False, num_spin_orbs, num_elec)
+                )
+                t += theta[counter] * tmp
             counter += 1
 
     if "d" in excitations:
         for (a, i, b, j) in iterate_T2(active_occ, active_unocc):
             if theta[counter] != 0.0:
-                tmp = a_op_spin(a, True, num_spin_orbs, num_elec).dot(a_op_spin(b, True, num_spin_orbs, num_elec))
-                tmp = tmp.dot(a_op_spin(j, False, num_spin_orbs, num_elec))
-                tmp = tmp.dot(a_op_spin(i, False, num_spin_orbs, num_elec))
-                t += theta[counter]*tmp
+                tmp = a_op_spin_matrix(a, True, num_spin_orbs, num_elec).dot(
+                    a_op_spin_matrix(b, True, num_spin_orbs, num_elec)
+                )
+                tmp = tmp.dot(a_op_spin_matrix(j, False, num_spin_orbs, num_elec))
+                tmp = tmp.dot(a_op_spin_matrix(i, False, num_spin_orbs, num_elec))
+                t += theta[counter] * tmp
             counter += 1
 
     T = t - np.conj(t).transpose()
-    return scipy.linalg.expm(T)
+    start = time.time()
+    A = scipy.linalg.expm(T)
+    print(f"expm: {time.time() - start}")
+    return A
