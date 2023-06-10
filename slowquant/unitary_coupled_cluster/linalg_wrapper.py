658001d7eaf9ecc3e9c340b50a80b5c894dea9c0
Erik Kjellgren <erikkjellgren@live.dk>
2023-06-10 15:58:15 +0200

    Corrected mistake

--- slowquant/unitary_coupled_cluster/linalg_wrapper.py
+++ slowquant/unitary_coupled_cluster/linalg_wrapper.py
@@ -1,9 +1,10 @@
 import numpy as np
-import scipy.sparse as ss
 import scipy
+import scipy.sparse as ss
+
 
 def matmul(A: np.ndarray | ss.csr_matrix, B: np.ndarray | ss.csr_matrix) -> np.ndarray | ss.csr_matrix:
-    if not isinstance(A, B):
+    if not isinstance(A, type(B)):
         print("A and B are not same matrix type.")
         print(f"A type: {type(A)}")
         print(f"B type: {type(B)}")
@@ -16,9 +17,10 @@ def matmul(A: np.ndarray | ss.csr_matrix, B: np.ndarray | ss.csr_matrix) -> np.n
         print(f"A got unsupported type: {type(A)}")
         exit()
 
-def expm(A: np.ndarray | ss.csr_matrix) -> np.ndarray | ss.csr_matrix: 
+
+def expm(A: np.ndarray | ss.csr_matrix) -> np.ndarray | ss.csr_matrix:
     if isinstance(A, np.ndarray):
-        return scipy.linalg.expm(A) 
+        return scipy.linalg.expm(A)
     elif isinstance(A, ss.csr_matrix):
         return ss.linalg.expm(A)
     else:
