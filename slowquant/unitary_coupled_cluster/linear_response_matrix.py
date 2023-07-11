6a15225cc927a7223d41f70a48c5da8229275488
Erik Kjellgren <erikkjellgren@live.dk>
2023-07-11 23:46:34 +0200

    clean up

--- slowquant/unitary_coupled_cluster/linear_response_matrix.py
+++ slowquant/unitary_coupled_cluster/linear_response_matrix.py
@@ -37,7 +37,6 @@ class LinearResponseUCCMatrix:
         wave_function: WaveFunctionUCC,
         excitations: str,
         is_spin_conserving: bool = False,
-        use_TDA: bool = False,
         do_selfconsistent_operators: bool = True,
     ) -> None:
         self.wf = copy.deepcopy(wave_function)
@@ -132,131 +131,131 @@ class LinearResponseUCCMatrix:
         idx_shift = len(self.q_ops)
         print("Gs", len(self.G_ops))
         print("qs", len(self.q_ops))
+        calculation_type = 'generic'
         for j, qJ in enumerate(self.q_ops):
             for i, qI in enumerate(self.q_ops):
                 if i < j:
                     continue
-                # print(i, "q,q")
-                # Make M
-                val = expectation_value_contracted(
-                    self.wf.state_vector, double_commutator_contract(qI.dagger, H, qJ), self.wf.state_vector
-                )
-                self.M[i, j] = self.M[j, i] = val
-                # Make Q
-                val = expectation_value_contracted(
-                    self.wf.state_vector,
-                    double_commutator_contract(qI.dagger, H, qJ.dagger),
-                    self.wf.state_vector,
-                )
-                self.Q[i, j] = self.Q[j, i] = val
-                # Make V
-                val = expectation_value_contracted(
-                    self.wf.state_vector,
-                    commutator_contract(qI.dagger, qJ),
-                    self.wf.state_vector,
-                )
-                self.V[i, j] = self.V[j, i] = val
-                # Make W
-                val = expectation_value_contracted(
-                    self.wf.state_vector,
-                    commutator_contract(qI.dagger, qJ.dagger),
-                    self.wf.state_vector,
-                )
-                self.W[i, j] = self.W[j, i] = val
+                if calculation_type == 'selfconsistent':
+                    None
+                elif calculation_type == 'naive':
+                    None
+                elif calculation_type == 'generic':
+                    # Make M
+                    self.M[i, j] = self.M[j, i] = expectation_value_contracted(
+                        self.wf.state_vector, double_commutator_contract(qI.dagger, H, qJ), self.wf.state_vector
+                    )
+                    # Make Q
+                    self.Q[i, j] = self.Q[j, i] = expectation_value_contracted(
+                        self.wf.state_vector,
+                        double_commutator_contract(qI.dagger, H, qJ.dagger),
+                        self.wf.state_vector,
+                    )
+                    # Make V
+                    self.V[i, j] = self.V[j, i] = expectation_value_contracted(
+                        self.wf.state_vector,
+                        commutator_contract(qI.dagger, qJ),
+                        self.wf.state_vector,
+                    )
+                    # Make W
+                    self.W[i, j] = self.W[j, i] = expectation_value_contracted(
+                        self.wf.state_vector,
+                        commutator_contract(qI.dagger, qJ.dagger),
+                        self.wf.state_vector,
+                    )
+                else:
+                    raise NameError('Could not determine calculation_type got: {calculation_type}')
         for j, GJ in enumerate(self.G_ops):
             for i, qI in enumerate(self.q_ops):
-                # print(i, "q,G")
-                # Make M
-                self.M[i, j + idx_shift] = expectation_value_contracted(
-                    self.wf.state_vector, double_commutator_contract(qI.dagger, H, GJ), self.wf.state_vector
-                )
-                # Make Q
-                self.Q[i, j + idx_shift] = expectation_value_contracted(
-                    self.wf.state_vector,
-                    double_commutator_contract(qI.dagger, H, GJ.dagger),
-                    self.wf.state_vector,
-                )
-                # Make V
-                self.V[i, j + idx_shift] = expectation_value_contracted(
-                    self.wf.state_vector, commutator_contract(qI.dagger, GJ), self.wf.state_vector
-                )
-                # Make W
-                self.W[i, j + idx_shift] = expectation_value_contracted(
-                    self.wf.state_vector, commutator_contract(qI.dagger, GJ.dagger), self.wf.state_vector
-                )
+                if calculation_type == 'selfconsistent':
+                    None
+                elif calculation_type == 'naive':
+                    None
+                elif calculation_type == 'generic':
+                    # Make M
+                    self.M[i, j + idx_shift] = expectation_value_contracted(
+                        self.wf.state_vector, double_commutator_contract(qI.dagger, H, GJ), self.wf.state_vector
+                    )
+                    # Make Q
+                    self.Q[i, j + idx_shift] = expectation_value_contracted(
+                        self.wf.state_vector,
+                        double_commutator_contract(qI.dagger, H, GJ.dagger),
+                        self.wf.state_vector,
+                    )
+                    # Make V
+                    self.V[i, j + idx_shift] = expectation_value_contracted(
+                        self.wf.state_vector, commutator_contract(qI.dagger, GJ), self.wf.state_vector
+                    )
+                    # Make W
+                    self.W[i, j + idx_shift] = expectation_value_contracted(
+                        self.wf.state_vector, commutator_contract(qI.dagger, GJ.dagger), self.wf.state_vector
+                    )
+                else:
+                    raise NameError('Could not determine calculation_type got: {calculation_type}')
         for j, qJ in enumerate(self.q_ops):
             for i, GI in enumerate(self.G_ops):
-                # print(i, "G,q")
-                # Make M
-                self.M[i + idx_shift, j] = expectation_value_contracted(
-                    self.wf.state_vector, double_commutator_contract(GI.dagger, H, qJ), self.wf.state_vector
-                )
-                # Make Q
-                self.Q[i + idx_shift, j] = expectation_value_contracted(
-                    self.wf.state_vector,
-                    double_commutator_contract(GI.dagger, H, qJ.dagger),
-                    self.wf.state_vector,
-                )
-                # Make V
-                self.V[i + idx_shift, j] = expectation_value_contracted(
-                    self.wf.state_vector, commutator_contract(GI.dagger, qJ), self.wf.state_vector
-                )
-                # Make W
-                self.W[i + idx_shift, j] = expectation_value_contracted(
-                    self.wf.state_vector, commutator_contract(GI.dagger, qJ.dagger), self.wf.state_vector
-                )
+                if calculation_type == 'selfconsistent':
+                    None
+                elif calculation_type == 'naive':
+                    None
+                elif calculation_type == 'generic':
+                    # Make M
+                    self.M[i + idx_shift, j] = expectation_value_contracted(
+                        self.wf.state_vector, double_commutator_contract(GI.dagger, H, qJ), self.wf.state_vector
+                    )
+                    # Make Q
+                    self.Q[i + idx_shift, j] = expectation_value_contracted(
+                        self.wf.state_vector,
+                        double_commutator_contract(GI.dagger, H, qJ.dagger),
+                        self.wf.state_vector,
+                    )
+                    # Make V
+                    self.V[i + idx_shift, j] = expectation_value_contracted(
+                        self.wf.state_vector, commutator_contract(GI.dagger, qJ), self.wf.state_vector
+                    )
+                    # Make W
+                    self.W[i + idx_shift, j] = expectation_value_contracted(
+                        self.wf.state_vector, commutator_contract(GI.dagger, qJ.dagger), self.wf.state_vector
+                    )
+                else:
+                    raise NameError('Could not determine calculation_type got: {calculation_type}')
         for j, GJ in enumerate(self.G_ops):
             for i, GI in enumerate(self.G_ops):
                 if i < j:
                     continue
-                # print(i, "G,G")
-                # Make M
-                self.M[i + idx_shift, j + idx_shift] = self.M[
-                    j + idx_shift, i + idx_shift
-                ] = expectation_value_contracted(
-                    self.wf.state_vector, double_commutator_contract(GI.dagger, H, GJ), self.wf.state_vector
-                )
-                # Make V
-                self.V[i + idx_shift, j + idx_shift] = self.V[
-                    j + idx_shift, i + idx_shift
-                ] = expectation_value_contracted(
-                    self.wf.state_vector, commutator_contract(GI.dagger, GJ), self.wf.state_vector
-                )
-                # Make Q
-                self.Q[i + idx_shift, j + idx_shift] = self.Q[
-                    j + idx_shift, i + idx_shift
-                ] = expectation_value_contracted(
-                    self.wf.state_vector,
-                    double_commutator_contract(GI.dagger, H, GJ.dagger),
-                    self.wf.state_vector,
-                )
-                # Make W
-                self.W[i + idx_shift, j + idx_shift] = self.W[
-                    j + idx_shift, i + idx_shift
-                ] = expectation_value_contracted(
-                    self.wf.state_vector, commutator_contract(GI.dagger, GJ.dagger), self.wf.state_vector
-                )
-        if False:
-            print("\n M matrix:")
-            for i in range(len(self.M)):
-                for j in range(i, len(self.M)):
-                    if abs(self.M[i, j]) > 10**-6:
-                        print("i,j, M[i,j]", i, j, self.M[i, j])
-            print("\n Q matrix:")
-            for i in range(len(self.M)):
-                for j in range(i, len(self.M)):
-                    if abs(self.Q[i, j]) > 10**-6:
-                        print("i,j, Q[i,j]", i, j, self.Q[i, j])
-            print("\n V matrix:")
-            for i in range(len(self.M)):
-                for j in range(i, len(self.M)):
-                    if abs(self.V[i, j]) > 10**-6:
-                        print("i,j, V[i,j]", i, j, self.V[i, j])
-            print("\n W matrix:")
-            for i in range(len(self.M)):
-                for j in range(i, len(self.M)):
-                    if abs(self.W[i, j]) > 10**-6:
-                        print("i,j, W[i,j]", i, j, self.W[i, j])
+                if calculation_type == 'selfconsistent':
+                    None
+                elif calculation_type == 'naive':
+                    None
+                elif calculation_type == 'generic':
+                    # Make M
+                    self.M[i + idx_shift, j + idx_shift] = self.M[
+                        j + idx_shift, i + idx_shift
+                    ] = expectation_value_contracted(
+                        self.wf.state_vector, double_commutator_contract(GI.dagger, H, GJ), self.wf.state_vector
+                    )
+                    # Make V
+                    self.V[i + idx_shift, j + idx_shift] = self.V[
+                        j + idx_shift, i + idx_shift
+                    ] = expectation_value_contracted(
+                        self.wf.state_vector, commutator_contract(GI.dagger, GJ), self.wf.state_vector
+                    )
+                    # Make Q
+                    self.Q[i + idx_shift, j + idx_shift] = self.Q[
+                        j + idx_shift, i + idx_shift
+                    ] = expectation_value_contracted(
+                        self.wf.state_vector,
+                        double_commutator_contract(GI.dagger, H, GJ.dagger),
+                        self.wf.state_vector,
+                    )
+                    # Make W
+                    self.W[i + idx_shift, j + idx_shift] = self.W[
+                        j + idx_shift, i + idx_shift
+                    ] = expectation_value_contracted(
+                        self.wf.state_vector, commutator_contract(GI.dagger, GJ.dagger), self.wf.state_vector
+                    )
+                else:
+                    raise NameError('Could not determine calculation_type got: {calculation_type}')
 
     def calc_excitation_energies(self) -> None:
         size = len(self.M)
@@ -280,19 +279,13 @@ class LinearResponseUCCMatrix:
     def get_excited_state_overlap(self, state_number: int) -> float:
         number_excitations = len(self.excitation_energies)
         norm = self.get_excited_state_norm(state_number)
-        normed_vec = self.response_vectors[:, state_number]*(1/norm)**0.5
-        print('WARNING: This function [get_excited_state_overlap] might not be working.')
+        normed_vec = self.response_vectors[:, state_number] * (1 / norm) ** 0.5
+        print("WARNING: This function [get_excited_state_overlap] might not be working.")
         for i, G in enumerate(self.q_ops + self.G_ops):
             if i == 0:
-                transfer_op = (
-                    normed_vec[i] * G.dagger
-                    + normed_vec[i + number_excitations] * G
-                )
+                transfer_op = normed_vec[i] * G.dagger + normed_vec[i + number_excitations] * G
             else:
-                transfer_op += (
-                    normed_vec[i] * G.dagger
-                    + normed_vec[i + number_excitations] * G
-                )
+                transfer_op += normed_vec[i] * G.dagger + normed_vec[i + number_excitations] * G
         return expectation_value_hybrid(self.wf.state_vector, transfer_op, self.wf.state_vector)
 
     def get_excited_state_norm(self, state_number: int) -> float:
@@ -309,25 +302,21 @@ class LinearResponseUCCMatrix:
                     + self.response_vectors[i + number_excitations, state_number] * G
                 )
         return expectation_value_hybrid(
-            self.wf.state_vector, transfer_op * transfer_op.dagger - transfer_op.dagger * transfer_op, self.wf.state_vector
+            self.wf.state_vector,
+            transfer_op * transfer_op.dagger - transfer_op.dagger * transfer_op,
+            self.wf.state_vector,
         )
 
     def get_transition_dipole(self, state_number: int, multipole_integral: np.ndarray) -> float:
         number_excitations = len(self.excitation_energies)
         assert number_excitations == len(self.q_ops) + len(self.G_ops)
         norm = self.get_excited_state_norm(state_number)
-        normed_vec = self.response_vectors[:, state_number]*(1/norm)**0.5
+        normed_vec = self.response_vectors[:, state_number] * (1 / norm) ** 0.5
         for i, G in enumerate(self.q_ops + self.G_ops):
             if i == 0:
-                transfer_op = (
-                    normed_vec[i] * G.dagger
-                    + normed_vec[i + number_excitations] * G
-                )
+                transfer_op = normed_vec[i] * G.dagger + normed_vec[i + number_excitations] * G
             else:
-                transfer_op += (
-                    normed_vec[i] * G.dagger
-                    + normed_vec[i + number_excitations] * G
-                )
+                transfer_op += normed_vec[i] * G.dagger + normed_vec[i + number_excitations] * G
         muz = one_electron_integral_transform(self.wf.c_trans, multipole_integral)
         counter = 0
         for p in range(self.wf.num_spin_orbs // 2):
@@ -348,21 +337,30 @@ class LinearResponseUCCMatrix:
                         self.wf.num_active_spin_orbs,
                         self.wf.num_virtual_spin_orbs,
                     )
-        return expectation_value_hybrid(self.wf.state_vector, muz_op * transfer_op - transfer_op * muz_op, self.wf.state_vector)
+        return expectation_value_hybrid(
+            self.wf.state_vector, muz_op * transfer_op - transfer_op * muz_op, self.wf.state_vector
+        )
 
     def get_oscillator_strength(self, state_number: int, multipole_integrals: np.ndarray) -> float:
         transition_dipole_x = self.get_transition_dipole(state_number, multipole_integrals[0])
         transition_dipole_y = self.get_transition_dipole(state_number, multipole_integrals[1])
         transition_dipole_z = self.get_transition_dipole(state_number, multipole_integrals[2])
         excitation_energy = self.excitation_energies[state_number]
-        return 2/3 * excitation_energy * (transition_dipole_x**2 +  transition_dipole_y**2 + transition_dipole_z**2)
+        return (
+            2
+            / 3
+            * excitation_energy
+            * (transition_dipole_x**2 + transition_dipole_y**2 + transition_dipole_z**2)
+        )
 
     def get_nice_output(self, multipole_integrals: np.ndarray) -> str:
-        output = "Excitation # | Excitation energy [Hartree] | Excitation energy [eV] | Oscillator strengths\n"
+        output = (
+            "Excitation # | Excitation energy [Hartree] | Excitation energy [eV] | Oscillator strengths\n"
+        )
         for i, exc_energy in enumerate(self.excitation_energies):
             osc_strength = self.get_oscillator_strength(i, multipole_integrals)
-            exc_str = f'{exc_energy:2.6f}'
-            exc_str_ev = f'{exc_energy*27.2114079527:3.6f}'
-            osc_str = f'{osc_strength:1.6f}'
+            exc_str = f"{exc_energy:2.6f}"
+            exc_str_ev = f"{exc_energy*27.2114079527:3.6f}"
+            osc_str = f"{osc_strength:1.6f}"
             output += f"{str(i+1).center(12)} | {exc_str.center(27)} | {exc_str_ev.center(22)} | {osc_str.center(20)}\n"
         return output
