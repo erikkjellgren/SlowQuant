e222f44dd0416da1a23a78c01b575938086360e6
ziechys <km.z@gmx.de>
2024-01-17 11:04:11 +0100

    Use folded operators in linear response

--- slowquant/qiskit_interface/linear_response.py
+++ slowquant/qiskit_interface/linear_response.py
@@ -40,7 +40,19 @@ class quantumLR:
                 # G2_2
                 self.G_ops.append(G2_2(a, b, i, j))
 
-        num_parameters = len(self.G_ops)
+        # q
+        self.q_ops = []
+        for p in range(0, WF.num_orbs):
+            for q in range(p + 1, WF.num_orbs):
+                if p in WF.inactive_idx and q in WF.inactive_idx:
+                    continue
+                if p in WF.virtual_idx and q in WF.virtual_idx:
+                    continue
+                if p in WF.active_idx and q in WF.active_idx:
+                    continue
+                self.q_ops.append(G1(p, q))
+
+        num_parameters = len(self.q_ops) + len(self.G_ops)
         self.A = np.zeros((num_parameters, num_parameters))
         self.B = np.zeros((num_parameters, num_parameters))
         self.Sigma = np.zeros((num_parameters, num_parameters))
@@ -57,16 +69,16 @@ class quantumLR:
             for i, GI in enumerate(self.G_ops[j:], j):
                 # Make A
                 self.A[i, j] = self.A[j, i] = QI.quantum_expectation_value(
-                    double_commutator(GI.dagger, self.H, GJ).get_folded_operator(
-                        self.WF.num_inactive_orbs, self.WF.num_active_orbs, self.WF.num_virtual_orbs
-                    )
+                    double_commutator(GI.dagger, self.H, GJ).get_folded_operator(*self.orbs)
                 )
                 # Make B
                 self.B[i, j] = self.B[j, i] = QI.quantum_expectation_value(
-                    double_commutator(GI.dagger, self.H, GJ.dagger)
+                    double_commutator(GI.dagger, self.H, GJ.dagger).get_folded_operator(*self.orbs)
                 )
                 # Make Sigma
-                self.Sigma[i, j] = self.Sigma[j, i] = QI.quantum_expectation_value(commutator(GI.dagger, GJ))
+                self.Sigma[i, j] = self.Sigma[j, i] = QI.quantum_expectation_value(
+                    commutator(GI.dagger, GJ).get_folded_operator(*self.orbs)
+                )
 
     def run_projected(
         self,
@@ -77,24 +89,20 @@ class quantumLR:
         G_exp = []
         HG_exp = []
         for j, GJ in enumerate(self.G_ops):
-            G_exp.append(QI.quantum_expectation_value(GJ))
-            HG_exp.append(QI.quantum_expectation_value(self.H * GJ))
+            G_exp.append(QI.quantum_expectation_value(GJ.get_folded_operator(*self.orbs)))
+            HG_exp.append(QI.quantum_expectation_value((self.H * GJ).get_folded_operator(*self.orbs)))
         for j, GJ in enumerate(self.G_ops):
             for i, GI in enumerate(self.G_ops[j:], j):
                 # Make A
-                val = QI.quantum_expectation_value(GI.dagger * self.H * GJ)
-                GG_exp = QI.quantum_expectation_value(
-                    (GI.dagger * GJ).get_folded_operator(
-                        self.WF.num_inactive_orbs, self.WF.num_active_orbs, self.WF.num_virtual_orbs
-                    )
-                )
-                val -= GG_exp * QI.vqe.electronic_energies[0]  # WF
+                val = QI.quantum_expectation_value((GI.dagger * self.H * GJ).get_folded_operator(*self.orbs))
+                GG_exp = QI.quantum_expectation_value((GI.dagger * GJ).get_folded_operator(*self.orbs))
+                val -= GG_exp * WF.elec_energy
                 val -= G_exp[i] * HG_exp[j]
-                val += G_exp[i] * G_exp[j] * QI.vqe.electronic_energies[0]
+                val += G_exp[i] * G_exp[j] * WF.elec_energy
                 self.A[i, j] = self.A[j, i] = val
                 # Make B
                 val = HG_exp[i] * G_exp[j]
-                val -= G_exp[i] * G_exp[j] * QI.vqe.electronic_energies[0]
+                val -= G_exp[i] * G_exp[j] * WF.elec_energy
                 self.B[i, j] = self.B[j, i] = val
                 # Make Sigma
                 self.Sigma[i, j] = self.Sigma[j, i] = GG_exp - (G_exp[i] * G_exp[j])
