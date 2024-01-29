04ab56cca8ca9ca5c303ea6ede5182e83faae957
ziechys <39458020+ziechys@users.noreply.github.com>
2024-01-29 14:09:22 +0100

    Update slowquant/qiskit_interface/linear_response/allprojected.py
    Co-authored-by: Erik Kjellgren <erikkjellgren@live.dk>

--- slowquant/qiskit_interface/linear_response/allprojected.py
+++ slowquant/qiskit_interface/linear_response/allprojected.py
@@ -11,10 +11,6 @@ from slowquant.qiskit_interface.operators import hamiltonian_pauli_2i_2a
 from slowquant.unitary_coupled_cluster.density_matrix import (
     ReducedDenstiyMatrix,
     get_orbital_gradient_response,
-    get_orbital_response_hessian_block,
-    get_orbital_response_metric_sigma,
-    get_orbital_response_property_gradient,
-    get_orbital_response_vector_norm,
 )
 
 
