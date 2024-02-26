8ce07ccfaebf26a084a70364899ac3c533a30a71
Oliver Dylander <oliver@Olivers-MacBook-Pro-3.local>
2024-02-26 14:41:18 +0100

    Updated code

---
+++ slowquant/qiskit_interface/linear_response_fullspace/lr_baseclass.py
@@ -0,0 +1,227 @@
+from collections.abc import Sequence
+
+import numpy as np
+import scipy
+from dmdm.util import iterate_t1_sa, iterate_t2_sa  # temporary solution
+
+from slowquant.qiskit_interface.operators import (
+    G1,
+    G2_1,
+    G2_2,
+    hamiltonian_pauli_0i_0a,
+)
+from slowquant.qiskit_interface.wavefunction import WaveFunction
+
+
+class quantumLRBaseClass:
+
+    def __init__(
+        self,
+        wf: WaveFunction,
+    ) -> None:
+        """Initialize linear response by calculating the needed matrices.
+
+        Args:
+            wf: Wavefunction object.
+        """
+        self.wf = wf
+        # Create operators
+        self.H_0i_0a = hamiltonian_pauli_0i_0a(wf.h_mo, wf.g_mo, wf.num_inactive_orbs, wf.num_active_orbs)
+
+        self.G_ops = []
+        # G1
+        for a, i, _, _, _ in iterate_t1_sa(wf.active_occ_idx, wf.active_unocc_idx):
+            self.G_ops.append(G1(a, i))
+        # G2
+        for a, i, b, j, _, _, op_id in iterate_t2_sa(wf.active_occ_idx, wf.active_unocc_idx):
+            if op_id > 0:
+                # G2_1
+                self.G_ops.append(G2_1(a, b, i, j))
+            else:
+                # G2_2
+                self.G_ops.append(G2_2(a, b, i, j))
+
+        num_parameters = len(self.G_ops)
+        self.num_params = num_parameters
+        self.num_G = len(self.G_ops)
+        self.A = np.zeros((num_parameters, num_parameters))
+        self.B = np.zeros((num_parameters, num_parameters))
+        self.Sigma = np.zeros((num_parameters, num_parameters))
+        self.Delta = np.zeros((num_parameters, num_parameters))
+
+        self.orbs = [self.wf.num_inactive_orbs, self.wf.num_active_orbs, self.wf.num_virtual_orbs]
+
+    def run(self) -> None:
+        """Run linear response."""
+        raise NotImplementedError
+
+    def _get_qbitmap(self) -> tuple[list[list[str]], list[list[str]], list[list[str]]]:
+        """Get qbitmapping of operators."""
+        raise NotImplementedError
+
+    def get_excitation_energies(self) -> np.ndarray:
+        """Solve LR eigenvalue problem."""
+        # Build Hessian and Metric
+        size = len(self.A)
+        self.Delta = np.zeros_like(self.Sigma)
+        self.hessian = np.zeros((size * 2, size * 2))
+        self.hessian[:size, :size] = self.A
+        self.hessian[:size, size:] = self.B
+        self.hessian[size:, :size] = self.B
+        self.hessian[size:, size:] = self.A
+        self.metric = np.zeros((size * 2, size * 2))
+        self.metric[:size, :size] = self.Sigma
+        self.metric[:size, size:] = self.Delta
+        self.metric[size:, :size] = -self.Delta
+        self.metric[size:, size:] = -self.Sigma
+
+        # Solve eigenvalue equation
+        eigval, eigvec = scipy.linalg.eig(self.hessian, self.metric)
+        sorting = np.argsort(eigval)
+        self.excitation_energies = np.real(eigval[sorting][size:])
+        self.excitation_vectors = np.real(eigvec[:, sorting][:, size:])
+
+        return self.excitation_energies
+
+    def get_normed_excitation_vectors(self) -> None:
+        """Get normed excitation vectors via excitated state norm."""
+        self.normed_excitation_vectors = np.zeros_like(self.excitation_vectors)
+        self._Z_G = self.excitation_vectors[: self.num_G, :]
+        self._Y_G = self.excitation_vectors[self.num_G :]
+        self._Z_G_normed = np.zeros_like(self._Z_G)
+        self._Y_G_normed = np.zeros_like(self._Y_G)
+
+        norms = self._get_excited_state_norm()
+        for state_number, norm in enumerate(norms):
+            if norm < 10**-10:
+                print(f"WARNING: State number {state_number} could not be normalized. Norm of {norm}.")
+                continue
+            self._Z_G_normed[:, state_number] = self._Z_G[:, state_number] * (1 / norm) ** 0.5
+            self._Y_G_normed[:, state_number] = self._Y_G[:, state_number] * (1 / norm) ** 0.5
+            self.normed_excitation_vectors[:, state_number] = (
+                self.excitation_vectors[:, state_number] * (1 / norm) ** 0.5
+            )
+
+    def _get_excited_state_norm(self) -> np.ndarray:
+        """Calculate the norm of excited states.
+
+        Returns:
+            Norm of excited states.
+        """
+        norms = np.zeros(len(self._Z_G[0]))
+        for state_number in range(len(self._Z_G[0])):
+            # Get Z_q Z_G Y_q and Y_G matrices
+            ZZG = np.outer(self._Z_G[:, state_number], self._Z_G[:, state_number].transpose())
+            YYG = np.outer(self._Y_G[:, state_number], self._Y_G[:, state_number].transpose())
+
+            norms[state_number] = np.sum(
+                self.metric
+                * (ZZG - YYG)
+            )
+
+        return norms
+
+    def get_transition_dipole(self, dipole_integrals: Sequence[np.ndarray]) -> np.ndarray:
+        """Calculate transtition dipole moment.
+
+        Args:
+            dipole_integrals: Dipole integrals (x,y,z) in AO basis.
+
+        Returns:
+            Transition dipole moments.
+        """
+        raise NotImplementedError
+
+    def get_oscillator_strength(self, dipole_integrals: Sequence[np.ndarray]) -> np.ndarray:
+        r"""Calculate oscillator strength.
+
+        .. math::
+            f_n = \frac{2}{3}e_n\left|\left<0\left|\hat{\mu}\right|n\left>\right|^2
+
+        Args:
+            dipole_integrals: Dipole integrals (x,y,z) in AO basis.
+
+        Rerturns:
+            Oscillator Strength.
+        """
+        transition_dipoles = self.get_transition_dipole(dipole_integrals)
+        osc_strs = np.zeros(len(transition_dipoles))
+        for idx, (excitation_energy, transition_dipole) in enumerate(
+            zip(self.excitation_energies, transition_dipoles)
+        ):
+            osc_strs[idx] = (
+                2
+                / 3
+                * excitation_energy
+                * (transition_dipole[0] ** 2 + transition_dipole[1] ** 2 + transition_dipole[2] ** 2)
+            )
+        self.oscillator_strengths = osc_strs
+        return osc_strs
+
+    def get_formatted_oscillator_strength(self) -> str:
+        """Create table of excitation energies and oscillator strengths.
+
+        Returns:
+            Nicely formatted table.
+        """
+        if not hasattr(self, "oscillator_strengths"):
+            raise ValueError(
+                "Oscillator strengths have not been calculated. Run get_oscillator_strength() first."
+            )
+
+        output = (
+            "Excitation # | Excitation energy [Hartree] | Excitation energy [eV] | Oscillator strengths\n"
+        )
+
+        for i, (exc_energy, osc_strength) in enumerate(
+            zip(self.excitation_energies, self.oscillator_strengths)
+        ):
+            exc_str = f"{exc_energy:2.6f}"
+            exc_str_ev = f"{exc_energy*27.2114079527:3.6f}"
+            osc_str = f"{osc_strength:1.6f}"
+            output += f"{str(i+1).center(12)} | {exc_str.center(27)} | {exc_str_ev.center(22)} | {osc_str.center(20)}\n"
+        return output
+
+
+def get_num_nonCBS(matrix: list[list[str]]) -> int:
+    """Count number of non computational basis measurements in operator matrix.
+
+    Args:
+        matrix: Operator matrix.
+
+    Returns:
+        Number of non computational basis measurements.
+    """
+    count = 0
+    dim = len(matrix[0])
+    for i in range(dim):
+        for j in range(i, dim):
+            for paulis in matrix[i][j]:
+                if any(letter in paulis for letter in ("X", "Y")):
+                    count += 1
+    return count
+
+
+def get_num_CBS_elements(matrix: list[list[str]]) -> tuple[int, int]:
+    """Count how many individual elements in matrix require only measurement in computational basis and how many do not.
+
+    Args:
+        matrix: Operator matrix.
+
+    Returns:
+        Number of elements only requiring computational basis measurements and how many do not.
+    """
+    count_CBS = 0
+    count_nCBS = 0
+    dim = len(matrix[0])
+    for i in range(dim):
+        for j in range(i, dim):
+            count_IM = 0
+            for paulis in matrix[i][j]:
+                if any(letter in paulis for letter in ("X", "Y")):
+                    count_IM += 1
+            if count_IM == 0 and not matrix[i][j] == "":
+                count_CBS += 1
+            elif count_IM > 0 and not matrix[i][j] == "":
+                count_nCBS += 1
+    return count_CBS, count_nCBS
---
+++ slowquant/qiskit_interface/linear_response_fullspace/naive.py
@@ -0,0 +1,109 @@
+from collections.abc import Sequence
+
+import numpy as np
+
+from slowquant.molecularintegrals.integralfunctions import (
+    one_electron_integral_transform,
+)
+from slowquant.qiskit_interface.base import FermionicOperator
+from slowquant.qiskit_interface.interface import make_cliques
+from slowquant.qiskit_interface.linear_response.lr_baseclass import (
+    get_num_CBS_elements,
+    get_num_nonCBS,
+    quantumLRBaseClass,
+)
+from slowquant.qiskit_interface.operators import (
+    commutator,
+    double_commutator,
+    one_elec_op_0i_0a,
+)
+
+
+class quantumLR(quantumLRBaseClass):
+    def run(
+        self,
+    ) -> None:
+        """Run simulation of naive LR matrix elements."""
+        idx_shift = self.num_q
+        print("Gs", self.num_G)
+
+        grad = np.zeros(2 * self.num_G)
+        for i, op in enumerate(self.G_ops):
+            grad[i] = self.wf.QI.quantum_expectation_value(
+                commutator(self.H_0i_0a, op).get_folded_operator(*self.orbs)
+            )
+            grad[i + self.num_G] = self.wf.QI.quantum_expectation_value(
+                commutator(op.dagger, self.H_0i_0a).get_folded_operator(*self.orbs)
+            )
+        if len(grad) != 0:
+            print("idx, max(abs(grad active)):", np.argmax(np.abs(grad)), np.max(np.abs(grad)))
+            if np.max(np.abs(grad)) > 10**-3:
+                print("WARNING: Large Gradient detected in G of ", np.max(np.abs(grad)))
+
+        # GG
+        for j, GJ in enumerate(self.G_ops):
+            for i, GI in enumerate(self.G_ops[j:], j):
+                # Make A
+                self.A[i + idx_shift, j + idx_shift] = self.A[j + idx_shift, i + idx_shift] = (
+                    self.wf.QI.quantum_expectation_value(
+                        double_commutator(GI.dagger, self.H_0i_0a, GJ).get_folded_operator(*self.orbs)
+                    )
+                )
+                # Make B
+                self.B[i + idx_shift, j + idx_shift] = self.B[j + idx_shift, i + idx_shift] = (
+                    self.wf.QI.quantum_expectation_value(
+                        double_commutator(GI.dagger, self.H_0i_0a, GJ.dagger).get_folded_operator(*self.orbs)
+                    )
+                )
+                # Make Sigma
+                self.Sigma[i + idx_shift, j + idx_shift] = self.Sigma[j + idx_shift, i + idx_shift] = (
+                    self.wf.QI.quantum_expectation_value(
+                        commutator(GI.dagger, GJ).get_folded_operator(*self.orbs)
+                    )
+                )
+
+    def get_transition_dipole(self, dipole_integrals: Sequence[np.ndarray]) -> np.ndarray:
+        """Calculate transition dipole moment.
+
+        Args:
+            dipole_integrals: Dipole integrals ordered as (x,y,z).
+
+        Returns:
+            Transition dipole moment.
+        """
+        if len(dipole_integrals) != 3:
+            raise ValueError(f"Expected 3 dipole integrals got {len(dipole_integrals)}")
+        number_excitations = len(self.excitation_energies)
+
+        mux = one_electron_integral_transform(self.wf.c_trans, dipole_integrals[0])
+        muy = one_electron_integral_transform(self.wf.c_trans, dipole_integrals[1])
+        muz = one_electron_integral_transform(self.wf.c_trans, dipole_integrals[2])
+        mux_op = one_elec_op_0i_0a(mux, self.wf.num_inactive_orbs, self.wf.num_active_orbs)
+        muy_op = one_elec_op_0i_0a(muy, self.wf.num_inactive_orbs, self.wf.num_active_orbs)
+        muz_op = one_elec_op_0i_0a(muz, self.wf.num_inactive_orbs, self.wf.num_active_orbs)
+        transition_dipole_x = 0.0
+        transition_dipole_y = 0.0
+        transition_dipole_z = 0.0
+        transition_dipoles = np.zeros((number_excitations, 3))
+        for state_number in range(number_excitations):
+            transfer_op = FermionicOperator({}, {})
+            for i, G in enumerate(self.G_ops):
+                transfer_op += (
+                    self._Z_G_normed[i, state_number] * G.dagger + self._Y_G_normed[i, state_number] * G
+                )
+
+            if self.num_G != 0:
+                transition_dipole_x = self.wf.QI.quantum_expectation_value(
+                    commutator(mux_op, transfer_op).get_folded_operator(*self.orbs)
+                )
+                transition_dipole_y = self.wf.QI.quantum_expectation_value(
+                    commutator(muy_op, transfer_op).get_folded_operator(*self.orbs)
+                )
+                transition_dipole_z = self.wf.QI.quantum_expectation_value(
+                    commutator(muz_op, transfer_op).get_folded_operator(*self.orbs)
+                )
+            transition_dipoles[state_number, 0] = q_part_x + transition_dipole_x
+            transition_dipoles[state_number, 1] = q_part_y + transition_dipole_y
+            transition_dipoles[state_number, 2] = q_part_z + transition_dipole_z
+
+        return transition_dipoles
