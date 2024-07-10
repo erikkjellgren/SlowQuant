251f4afaaf2c2f8232da14520edc028c7f2e40e2
Erik Kjellgren <erikkjellgren@live.dk>
2024-07-10 14:01:45 +0200

    Reintroduced outcommented lines

--- slowquant/unitary_coupled_cluster/linear_response/lr_baseclass.py
+++ slowquant/unitary_coupled_cluster/linear_response/lr_baseclass.py
@@ -132,8 +132,8 @@ class LinearResponseBaseClass:
             _,
         ) = np.linalg.eig(E2)
         print(f"Smallest Hessian eigenvalue: {np.min(hess_eigval)}")
-        # if np.min(hess_eigval) < 0:
-        #    raise ValueError("Negative eigenvalue in Hessian.")
+        if np.min(hess_eigval) < 0:
+            raise ValueError("Negative eigenvalue in Hessian.")
 
         S = np.zeros((size * 2, size * 2))
         S[:size, :size] = self.Sigma
