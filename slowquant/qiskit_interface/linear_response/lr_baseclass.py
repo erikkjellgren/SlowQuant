2cb9f39f76b33d51fbebc2141b3b43c59c94c22b
Karl Michael Ziems <39458020+ziechys@users.noreply.github.com>
2024-07-11 14:41:42 +0200

    Update slowquant/qiskit_interface/linear_response/lr_baseclass.py
    Co-authored-by: Erik Kjellgren <erikkjellgren@live.dk>

--- slowquant/qiskit_interface/linear_response/lr_baseclass.py
+++ slowquant/qiskit_interface/linear_response/lr_baseclass.py
@@ -113,7 +113,7 @@ class quantumLRBaseClass:
         """Get standard deviation in matrix elements of LR equation.
 
         Args:
-            no_coeffs:  Boolean to no include coefficiants
+            no_coeffs:  Boolean to no include coefficients
             verbose:    Boolean to print more info
             cv:         Boolean to calculate coefficient of variance
             save:       Boolean to save operator-specific standard deviations
