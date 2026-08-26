6227ae50d781110fd40ed10d4702abc5b0a452d4
JulianeFuglsbjerg <juliane@DESKTOP-HGQJOA3.localdomain>
2026-08-26 10:43:13 +0200

    q part of 2e integrals in property gradient

--- slowquant/unitary_coupled_cluster/linear_response/naive.py
+++ slowquant/unitary_coupled_cluster/linear_response/naive.py
@@ -2,13 +2,15 @@ import numpy as np
 
 from slowquant.molecularintegrals.integralfunctions import (
     one_electron_integral_transform,
+    two_electron_integral_transform,
 )
 from slowquant.unitary_coupled_cluster.density_matrix import (
     get_orbital_gradient_response,
     get_orbital_response_hessian_block,
     get_triplet_orbital_response_hessian_block,
     get_orbital_response_metric_sigma,
-    get_orbital_response_property_gradient,
+    get_orbital_response_property_gradient_1e,
+    get_orbital_response_property_gradient_2e,
 )
 from slowquant.unitary_coupled_cluster.linear_response.lr_baseclass import (
     LinearResponseBaseClass,
@@ -329,38 +331,57 @@ class LinearResponse(LinearResponseBaseClass):
                 )
                 self.Sigma[i + idx_shift, j + idx_shift] = self.Sigma[j + idx_shift, i + idx_shift] = val
 
-    def get_property_gradient(self, property_integrals: np.ndarray | tuple[np.ndarray]) -> np.ndarray:
+    def get_property_gradient(self, prop_int_1e: np.ndarray, prop_int_2e: np.ndarray | None = None) -> np.ndarray:
         """Calculate property gradient.
 
         Args:
-            property_integrals: Integrals in AO basis.
+            prop_int_1e: one-electron property integrals in AO basis.
+            prop_int_2e: two-electron property integrals in AO basis.
 
         Returns:
             Property gradient.
         """
+
+        if not self.triplet:
+            E = Epq
+        else:
+            if prop_int_2e is not None:
+                raise ValueError("Got triplet response and two-electron integral in the property gradient. Cannot be done simultaniously.")
+            E = Tpq
+
         size_mo = self.wf.num_inactive_orbs + self.wf.num_active_orbs + self.wf.num_virtual_orbs
-        num_mo = len(property_integrals)
-        mo = np.zeros((num_mo, size_mo, size_mo))
-        for i, ao in enumerate(property_integrals):
-            mo[i, :, :] += one_electron_integral_transform(self.wf.c_mo, ao)
+        num_mo = len(prop_int_1e)
+        mo_1e = np.zeros((num_mo, size_mo, size_mo))
+        for i, ao in enumerate(prop_int_1e):
+            mo_1e[i, :, :] += one_electron_integral_transform(self.wf.c_mo, ao)
+
+        if prop_int_2e is not None:
+            mo_2e = np.zeros((num_mo, size_mo, size_mo, size_mo, size_mo))
+            for i, ao in enumerate(prop_int_2e):
+                mo_2e[i, :, :] += two_electron_integral_transform(self.wf.c_mo, ao)
 
         idx_shift_q = len(self.q_ops)
         V = np.zeros((len(self.q_ops + self.G_ops), num_mo))
 
         if len(self.q_ops) != 0:
             # Orbital response part
-            V[:idx_shift_q, :] = get_orbital_response_property_gradient(
-                mo,
+            V[:idx_shift_q, :] = get_orbital_response_property_gradient_1e(
+                mo_1e,
                 self.wf.kappa_no_activeactive_idx,
                 self.wf.num_inactive_orbs,
                 self.wf.num_active_orbs,
                 self.wf.rdm1,
             )
-        
-        if not self.triplet:
-            E = Epq
-        else:
-            E = Tpq
+
+            if prop_int_2e is not None:
+                V[:idx_shift_q, :] = get_orbital_response_property_gradient_2e(
+                        mo_2e,
+                        self.wf.kappa_no_activeactive_idx,
+                        self.wf.num_inactive_orbs,
+                        self.wf.num_active_orbs,
+                        self.wf.rdm1,
+                        self.wf.rdm2,
+                    )
 
         for idx, G in enumerate(self.G_ops):
             G_ket = propagate_state([G], self.wf.ci_coeffs, *self.index_info)
@@ -382,7 +403,7 @@ class LinearResponse(LinearResponseBaseClass):
                     G_ket, 
                     *self.index_info
                 ) 
-                V[idx + idx_shift_q, :] += mo[:, i, i] * val
+                V[idx + idx_shift_q, :] += mo_1e[:, i, i] * val
             # Active part
             for v in range(self.wf.num_inactive_orbs, self.wf.num_inactive_orbs + self.wf.num_active_orbs):
                 for w in range(
@@ -404,7 +425,7 @@ class LinearResponse(LinearResponseBaseClass):
                         G_ket, 
                         *self.index_info
                     )
-                    V[idx + idx_shift_q, :] += mo[:, v, w] * val
-        if np.allclose(mo, mo.transpose(0, -1, -2)):
+                    V[idx + idx_shift_q, :] += mo_1e[:, v, w] * val
+        if np.allclose(mo_1e, mo_1e.transpose(0, -1, -2)):
             return np.vstack((V, -1 * V))
         return np.vstack((V, V))
